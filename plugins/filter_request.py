da5a923061c4ee84d493f8b5f527641f96e1d4a4
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-11-27 11:11:36 +0700

    Update filter_request.py

--- plugins/filter_request.py
+++ plugins/filter_request.py
@@ -24,15 +24,22 @@ async def start(_, message):
             ]
     await message.reply_text(text=random.choice(pesan))
 
-@Client.on_message(filters.regex(r"request|req", re.I) & (filters.text | filters.caption) & filters.chat(-1001201566570))
+@Client.on_message(filters.regex(r"#request|#req", re.I) & (filters.text | filters.photo) & filters.chat(-1001201566570))
 async def request_user(client, message):
     markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="💬 Lihat Pesan", url=f"https://t.me/c/1201566570/{message.message_id}")],
                                    [InlineKeyboardButton(text="🚫 Tolak", callback_data=f"rejectreq_{message.message_id}_{message.chat.id}"),InlineKeyboardButton(text="✅ Done", callback_data=f"donereq_{message.message_id}_{message.chat.id}")],
                                    [InlineKeyboardButton(text="✖️ Tidak Tersedia", callback_data=f"unavailablereq_{message.message_id}_{message.chat.id}")]
                                  ])
-    forward = await client.send_message(-1001575525902, f"Request by <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a>\n\n{message.text}", reply_markup=markup)
-    markup2 = InlineKeyboardMarkup([[InlineKeyboardButton(text="⏳ Cek status request", url=f"https://t.me/c/1575525902/{forward.message_id}")]])
-    await message.reply_text(text=f"Request kamu sudah dikirim ke admin yaa..", quote=True, reply_markup=markup2)
+    try:
+      if message.text:
+        forward = await client.send_message(-1001575525902, f"Request by <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a>\n\n{message.text}", reply_markup=markup)
+        markup2 = InlineKeyboardMarkup([[InlineKeyboardButton(text="⏳ Cek status request", url=f"https://t.me/c/1575525902/{forward.message_id}")]])
+      if message.photo:
+        forward = await client.send_photo(-1001575525902, message.photo.file_id, caption=f"Request by <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a>\n\n{message.caption}", reply_markup=markup)
+        markup2 = InlineKeyboardMarkup([[InlineKeyboardButton(text="⏳ Cek status request", url=f"https://t.me/c/1575525902/{forward.message_id}")]])
+      await message.reply_text(text=f"Request kamu sudah dikirim ke admin yaa..", quote=True, reply_markup=markup2)
+    except:
+      pass
 
 @Client.on_callback_query(filters.regex(r"^donereq"))
 async def _callbackreq(c: Client, q: CallbackQuery):
