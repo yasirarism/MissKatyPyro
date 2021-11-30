a36b3227b9a9d09cd85f4c7ea54c1627ec308b84
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-01 06:05:03 +0700

    Update nightmode.py

--- bot/plugins/nightmode.py
+++ bot/plugins/nightmode.py
@@ -4,20 +4,21 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import logging
 import pytz
 import requests
+from bot import app
 from datetime import datetime
 
-async def job_close(client):
+async def job_close():
     now = datetime.now(pytz.timezone('Asia/Jakarta'))
     days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']
     month = ['Unknown', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
     tgl = now.strftime('%d')
     tahun = now.strftime('%Y')
     jam = now.strftime('%H:%M')
-    await client.send_sticker(-1001128045651, "CAACAgQAAxkDAAEDfNhgygZBqbTlbOQ6Gk3CmtD-bnkRDAACLxsAAvEGNAY-qWSFYAqy3R4E")
-    await client.send_message(
+    await app.send_sticker(-1001128045651, "CAACAgQAAxkDAAEDfNhgygZBqbTlbOQ6Gk3CmtD-bnkRDAACLxsAAvEGNAY-qWSFYAqy3R4E")
+    await app.send_message(
       -1001128045651, "📆 "+days[now.weekday()]+", "+tgl+" "+month[now.month]+" "+tahun+"\n⏰ Jam : "+jam+"\n\n**🌗 Mode Malam Aktif**\n`Proses LockDown dimulai, Grup ditutup dan semua member tidak akan bisa mengirim pesan. Selamat beristirahat dan bermimpi indah !!`\n\n~ Dbuat dengan Pyrogram v1.2.9.."
     )
-    await client.set_chat_permissions(-1001128045651, ChatPermissions(can_send_messages=False, can_invite_users=True)
+    await app.set_chat_permissions(-1001128045651, ChatPermissions(can_send_messages=False, can_invite_users=True)
     )
 
 async def job_close_ymoviez():
@@ -27,13 +28,13 @@ async def job_close_ymoviez():
     tgl = now.strftime('%d')
     tahun = now.strftime('%Y')
     jam = now.strftime('%H:%M')
-    await client.send_message(
+    await app.send_message(
       -1001255283935, "📆 "+days[now.weekday()]+", "+tgl+" "+month[now.month]+" "+tahun+"\n⏰ Jam : "+jam+"\n\n**🌗 Mode Malam Aktif**\n`Grup ditutup hingga jam 6 pagi. Selamat beristirahat.....`\n\n~ Dbuat dengan Pyrogram v1.2.9.."
     )
-    await client.set_chat_permissions(-1001255283935, ChatPermissions(can_send_messages=False, can_invite_users=True)
+    await app.set_chat_permissions(-1001255283935, ChatPermissions(can_send_messages=False, can_invite_users=True)
     )
 
-async def job_open(client):
+async def job_open():
     now = datetime.now(pytz.timezone('Asia/Jakarta'))
     days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']
     month = ['Unknown', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
@@ -44,14 +45,14 @@ async def job_open(client):
     json = req.json()
     quote = json["result"]["quote"]
     by = json["result"]["by"]
-    await client.send_sticker(-1001128045651, "CAACAgQAAxkDAAEDeJhgyLPTe0shLKykbafLA-rZk3CYZAAC4xoAAvEGNAYXtspUoZE5Nx4E")
-    await client.send_message(
+    await app.send_sticker(-1001128045651, "CAACAgQAAxkDAAEDeJhgyLPTe0shLKykbafLA-rZk3CYZAAC4xoAAvEGNAYXtspUoZE5Nx4E")
+    await app.send_message(
         -1001128045651, "📆 "+days[now.weekday()]+", "+tgl+" "+month[now.month]+" "+tahun+"\n⏰ "+jam+"`\n\n🌗 Mode Malam Selesai\nSelamat pagi, grup kini telah dibuka semoga hari-harimu menyenangkan.`\n\n**Quotes Today:**\n"+quote+" ~"+by
     )
-    await client.set_chat_permissions(-1001128045651, ChatPermissions(can_send_messages=True, can_send_media_messages=True, can_send_stickers=False, can_send_animations=True, can_invite_users=True, can_add_web_page_previews=True, can_use_inline_bots=True)
+    await app.set_chat_permissions(-1001128045651, ChatPermissions(can_send_messages=True, can_send_media_messages=True, can_send_stickers=False, can_send_animations=True, can_invite_users=True, can_add_web_page_previews=True, can_use_inline_bots=True)
     )
 
-async def job_open_ymoviez(client):
+async def job_open_ymoviez():
     now = datetime.now(pytz.timezone('Asia/Jakarta'))
     days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']
     month = ['Unknown', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
@@ -62,10 +63,10 @@ async def job_open_ymoviez(client):
     json = req.json()
     by = json["result"]["by"]
     quote = json["result"]["quote"]
-    await client.send_message(
+    await app.send_message(
         -1001255283935, "📆 "+days[now.weekday()]+", "+tgl+" "+month[now.month]+" "+tahun+"\n⏰ "+jam+"`\n\n🌗 Mode Malam Selesai\nSelamat pagi, grup kini telah dibuka semoga hari-harimu menyenangkan.`\n\n**Quotes Today:**\n"+quote+" ~"+by
     )
-    await client.set_chat_permissions(-1001255283935, ChatPermissions(can_send_messages=True, can_send_media_messages=True, can_send_stickers=True, can_send_animations=True, can_invite_users=True, can_add_web_page_previews=True, can_use_inline_bots=True)
+    await app.set_chat_permissions(-1001255283935, ChatPermissions(can_send_messages=True, can_send_media_messages=True, can_send_stickers=True, can_send_animations=True, can_invite_users=True, can_add_web_page_previews=True, can_use_inline_bots=True)
     )
 
 scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
