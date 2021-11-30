c8cad983eb3cfe31f818131c0f86d5ab97a120ea
Yasir <yasiramunandar@gmail.com>
2021-11-30 12:42:21 +0000

    tes

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,7 +5,4 @@ from pyrogram import idle, __version__
 
 # Run Bot
 if __name__ == "__main__":
-    app.start()
-    me = app.get_me()
-    logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-    idle()
+       YasirBot.run()
