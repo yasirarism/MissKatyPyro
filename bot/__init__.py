5d30517bf90b49414ca724e49964b21a8d95c00b
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-01 06:08:35 +0700

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,23 @@
+import logging
+import logging.config
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN
+from utils import temp
+
+app = Client(
+    session_name=SESSION,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=50,
+    plugins=dict(root="bot/plugins"),
+    sleep_threshold=5,
+)
