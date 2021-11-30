a305271db8b6fe1fad01ad0c797586ab00345c92
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-11-30 10:22:03 +0000

    Create json.py

---
+++ plugins/json.py
@@ -0,0 +1,26 @@
+import os
+from pyrogram import Client, filters
+from info import COMMAND_HANDLER
+
+@Client.on_message(filters.command(["json","json@MissKatyRoBot"], COMMAND_HANDLER))
+async def jsonify(_, message):
+    the_real_message = None
+    reply_to_id = None
+
+    if message.reply_to_message:
+        the_real_message = message.reply_to_message
+    else:
+        the_real_message = message
+
+    try:
+        await message.reply_text(f"<code>{the_real_message}</code>")
+    except Exception as e:
+        with open("json.text", "w+", encoding="utf8") as out_file:
+            out_file.write(str(the_real_message))
+        await message.reply_document(
+            document="json.text",
+            caption=str(e),
+            disable_notification=True,
+            reply_to_message_id=reply_to_id
+        )
+        os.remove("json.text")
