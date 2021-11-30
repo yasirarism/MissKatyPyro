15343ec0e56ba4b8ee288dfd60d803d37c15376e
Yasir <yasiramunandar@gmail.com>
2021-11-30 13:04:54 +0000

    tes

--- bot/__init__.py
+++ bot/__init__.py
@@ -7,7 +7,7 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
-from pyrogram import Client
+from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
