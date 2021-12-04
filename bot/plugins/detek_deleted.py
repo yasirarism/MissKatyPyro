0600d2217464f92416abb982ad90ac4929036dc9
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-04 21:23:20 +0700

    Update detek_deleted.py

--- bot/plugins/detek_deleted.py
+++ bot/plugins/detek_deleted.py
@@ -6,5 +6,5 @@ from typing import List
 
 @Client.on_deleted_messages(filters.group)
 async def deleted(client: Client, messages: List[Message]):
-    tmp = "".join(msg.message_id for msg in msgs.messages)
-    await client.send_message(messages.chat.id, temp)
+    tmp = "".join(msg.message_id for msg in messages)
+    await client.send_message(messages.chat.id, tmp)
