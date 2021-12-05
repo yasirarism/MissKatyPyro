bc76c16aa32da61db1b977a99fce59a7b308f999
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-05 07:04:11 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,5 @@
 import logging
-from bot import app
+from bot import app, user
 from utils import temp
 from pyrogram.raw.all import layer
 from pyrogram import idle, __version__
@@ -7,10 +7,13 @@ from pyrogram import idle, __version__
 # Run Bot
 if __name__ == "__main__":
     app.start()
+    user.start
     me = app.get_me()
+    user = user.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
     app.send_message(617426792, f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
     logging.info(f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
+    logging.info(f"{user.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{user.username}.")
     idle()
