7e8c429bad41f402158318fe1eb151ccff591c4c
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-05 07:29:30 +0700

    Update detek_deleted.py

--- bot/plugins/detek_deleted.py
+++ bot/plugins/detek_deleted.py
@@ -6,6 +6,6 @@ from typing import List
 from bot import user, app
 
 @user.on_deleted_messages()
-async def del_msg(client: Client, messages: List[Message]):
-    for msg in messages:
-       await app.send_message(617426792, msg)
+async def del_msg(client, message):
+    #for msg in messages:
+    await app.send_message(617426792, message)
