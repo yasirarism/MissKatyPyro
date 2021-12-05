2f9fbf1040930d3693205f514de0503f9c0d7b47
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-05 11:28:18 +0700

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -9,4 +9,4 @@ async def ping(_, message):
     rm = await message.reply_text("🐱 Pong!!...")
     end_t = time.time()
     time_taken_s = round(end_t - start_t, 3)
-    await rm.edit(f"<b>🐈 Pong!</b>\n<code>{time_taken_s} detik</code>\n\n<b>Userbot:</b> <a href='https://t.me/{(await user.get_me()).username}'>{(await user.get_me()).first_name}</a>\n<b>Bot:</b> {(await app.get_me()).username}")
+    await rm.edit(f"<b>🐈 Pong!</b>\n<code>{time_taken_s} detik</code>\n\n<b>Userbot:</b> <a href='https://t.me/{(await user.get_me()).username}'>{(await user.get_me()).first_name}</a>\n<b>Bot:</b> {(await app.get_me()).username}", disable_web_page_preview=True)
