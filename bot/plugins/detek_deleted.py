8a365da3fda346016f3c5e38d100f08fb4d51edb
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-05 07:10:17 +0700

    Update detek_deleted.py

--- bot/plugins/detek_deleted.py
+++ bot/plugins/detek_deleted.py
@@ -3,9 +3,9 @@ from pyrogram.types import (
     Message
 )
 from typing import List
-from bot import user
+from bot import user, app
 
 @user.on_deleted_messages()
 async def del_msg(client: Client, messages: List[Message]):
     for msg in messages:
-       await client.send_message(617426792, msg)
+       await app.send_message(617426792, msg)
