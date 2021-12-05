1bc9becab4d2d54ab4060af592c394913586aa93
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-05 11:20:17 +0700

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -1,11 +1,12 @@
 import time
-from pyrogram import filters, Client
+from pyrogram import filters
 from info import COMMAND_HANDLER
+from bot import app, user
 
-@Client.on_message(filters.command("ping", COMMAND_HANDLER))
+@app.on_message(filters.command("ping", COMMAND_HANDLER))
 async def ping(_, message):
     start_t = time.time()
     rm = await message.reply_text("🐱 Pong!!...")
     end_t = time.time()
     time_taken_s = round(end_t - start_t, 3)
-    await rm.edit(f"<b>🐈 Pong!</b>\n<code>{time_taken_s} detik</code>")
+    await rm.edit(f"<b>🐈 Pong!</b>\n<code>{time_taken_s} detik</code>\n\n<b>Userbot:</b> <a href='https://t.me/{await user.get_me().username}'>Yasir Aris M</a>\n<b>Bot:</b> {await app.get_me().username}")
