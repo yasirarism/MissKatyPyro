6aa7c3bf16ba18153eea5cbf16efab0cf1c77060
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-07 13:30:13 +0700

    Update copy_forward.py

--- bot/plugins/copy_forward.py
+++ bot/plugins/copy_forward.py
@@ -7,10 +7,14 @@ async def copy(client, message):
     try:
         to = message.text.split(" ")[1]
         reply = message.reply_to_message
-        if not reply and not to:
-          return await message.reply_text("Silahkan balas pesan yang mau dicopy, lalu kirim command /copy [chat_tujuan]")
-        await client.copy_message(to, message.chat.id, message.reply_to_message.message_id, reply_markup=message.reply_to_message.reply_markup)
-        await message.reply_text("Pesan berhasil dikirim..")
+        user = await client.get_chat_member(-1001404537486, message.from_user.id)
+        if user.status in ['administrator','creator']:
+           if not reply and not to:
+              return await message.reply_text("Silahkan balas pesan yang mau dicopy, lalu kirim command /copy [chat_tujuan]")
+           await client.copy_message(to, message.chat.id, message.reply_to_message.message_id, reply_markup=message.reply_to_message.reply_markup)
+           await message.reply_text("Pesan berhasil dikirim..")
+        else:
+           await message.reply_text("😝😝😝")
     except Exception as e:
         await message.reply_text(e)
 
@@ -19,9 +23,13 @@ async def forward(client, message):
     try:
         to = message.text.split(" ")[1]
         reply = message.reply_to_message
-        if not reply and not to:
-          return await message.reply_text("Silahkan balas pesan yang mau diforward, lalu kirim command /copy [chat_tujuan]")
-        await client.forward_messages(to, message.chat.id, message.reply_to_message.message_id)
-        await message.reply_text("Pesan berhasil dikirim..")
+        user = await client.get_chat_member(-1001404537486, message.from_user.id)
+        if user.status in ['administrator','creator']:
+           if not reply and not to:
+              return await message.reply_text("Silahkan balas pesan yang mau diforward, lalu kirim command /forward [chat_tujuan]")
+           await client.forward_messages(to, message.chat.id, message.reply_to_message.message_id)
+           await message.reply_text("Pesan berhasil dikirim..")
+        else:
+           await message.reply_text("😝😝😝")
     except Exception as e:
         await message.reply_text(e)
