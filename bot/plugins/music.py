5cbb608331511611ea510d4555184ed8c6005a81
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-09 22:14:33 +0700

    Create music.py

---
+++ bot/plugins/music.py
@@ -0,0 +1,43 @@
+from pyrogram import filters
+from nksama import app, user
+import pytgcalls
+
+
+calls = pytgcalls.GroupCallFactory(user).get_group_call()
+
+
+@app.on_message(filters.command('play'))
+async def play(_,message):
+  try:
+    await user.start()
+  except:
+    pass
+  reply = message.reply_to_message
+  if reply:
+    fk = await message.reply('Sedang mendownload....')
+    path = await reply.download()
+    await calls.join(message.chat.id)
+    await calls.start_audio(path , repeat=False)
+    await fk.edit('playing...')
+    
+@app.on_message(filters.command('vplay'))
+async def vplay(_,message):
+  try:
+    await user.start()
+  except:
+    pass
+  reply = message.reply_to_message
+  if reply:
+    path = await reply.download()
+    await calls.join(message.chat.id)
+    await calls.start_video(path , repeat=False)
+    await fk.edit('playing...')
+
+@app.on_message(filters.command('leavevc'))
+async def leavevc(_,message):
+  await calls.stop()
+  await calls.leave_current_group_call()
+
+@app.on_message(filters.command('pause'))
+async def pause(_,message):
+  await calls.pause_stream()
