e756cb565e01cba89135d4d658b07f1c04170302
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-11 02:04:10 +0000

    Update ocr.py

--- bot/plugins/ocr.py
+++ bot/plugins/ocr.py
@@ -17,7 +17,7 @@ async def ocr(_, message):
     response = upload_file(file_path)
     url = f"https://telegra.ph{response[0]}"
     req = requests.get(f"https://script.google.com/macros/s/AKfycbwmaiH74HX_pL-iNzw8qUsHoDMtBIBLogclgLD6cNLpPM6piGg/exec?url={url}").json()
-    await msg.edit(re)
+    await msg.edit(req)
     os.remove(file_path)
   except Exception as e:
     await msg.edit(str(e))
