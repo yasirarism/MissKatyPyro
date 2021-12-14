2e374661f987824ab00bc0682c70920dac778444
Yasir <yasiramunandar@gmail.com>
2021-12-14 02:57:08 +0000

    tes

--- database/__init__.py
+++
@@ -1,9 +0,0 @@
-"""EduuRobot Database!"""
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2018-2021 Amano Team
-
-from typing import List
-
-from .tes_sqlite import db, dbc
-
-__all__: List[str] = ["db", "dbc"]
