a9433082cf10189b23c89920e46223d6f148efc9
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-17 03:39:01 +0000

    Update copy_forward.py

--- bot/plugins/copy_forward.py
+++ bot/plugins/copy_forward.py
@@ -1,5 +1,5 @@
 from pyrogram import filters, Client
-from pyrogram.errors import PeerIdInvalid
+from pyrogram.errors import PeerIdInvalid, UserIsBlocked
 from info import COMMAND_HANDLER
 
 @Client.on_message(filters.command(["copy","copy@MissKatyRoBot"], COMMAND_HANDLER))
@@ -12,12 +12,18 @@ async def copy(client, message):
            if not reply and not to:
               return await message.reply_text("Silahkan balas pesan yang mau dicopy, lalu kirim command /copy [chat_tujuan]")
            await client.copy_message(to, message.chat.id, message.reply_to_message.message_id, reply_markup=message.reply_to_message.reply_markup)
-           await message.reply_text("Pesan berhasil dikirim..")
+           return await message.reply_text("Pesan berhasil dikirim..")
         else:
-           await message.reply_text("😝😝😝")
-    except Exception as e:
-        await message.reply_text(e)
-
+           return await message.reply_text("😝😝😝")
+    except IndexError:
+        if not message.reply_to_message:
+            return await message.reply_text("Silahkan balas pesan yang mau dicopy ke chat pribadi.")
+        try:
+            await client.copy_message(message.from_user.id, message.chat.id, message.reply_to_message.message_id, reply_markup=message.reply_to_message.reply_markup)
+            return await message.reply_text("Pesan berhasil dikirim..")
+        except UserIsBlocked:
+            await message.reply_text("Silahkan PM Saya untuk mengcopy pesan ke chat pribadi..")
+            
 @Client.on_message(filters.command(["forward","forward@MissKatyRoBot"], COMMAND_HANDLER))
 async def forward(client, message):
     try:
@@ -32,4 +38,4 @@ async def forward(client, message):
         else:
            await message.reply_text("😝😝😝")
     except Exception as e:
-        await message.reply_text(e)
+        pass
