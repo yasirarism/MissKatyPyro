82a215275c000fe16c799bb51271fd2a59f19f5a
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-26 07:14:48 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,6 +11,7 @@ from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN
+from telethon import TelegramClient
 from utils import temp
 
 SESSION_STRING = "BQAgpCukd2qCITQXeGlTD5ERryFLNJZtL_uxlpMdHQifPOG-STbh4FMXJJi-sq9Q8fK49ptAUkewjkldOFrkeD4MffXUL7ByPQXOwYZQ4vCsSiKMEF44j0hbboZASz2ImYy2lFUtRpmeRre-kbdHRBFx1bX2NXAfsXKyQy7LUhZQCU3TdwZIRQpxzA6d15osugQ8ofJ5LaJNj7xrLVvYEzXxVhl9HyQ5wlK8qYUhrvOKSopgK1J1KmEVk4jwgBpKraLCii76RYojtpf0KZwLsUT8mGtzXKUjl5_m320b4W-ECUW2jlsKe6f_qgmKnyqinYPDK1ZatouWvQxpVriUMRbPJM0vaAA"
@@ -30,4 +31,10 @@ user = Client(
     session_name=SESSION_STRING,
     api_id=API_ID,
     api_hash=API_HASH,
+)
+
+telethon = TelegramClient(
+        session_name="Telethon Bot",
+        api_id=API_ID,
+        api_hash=API_HASH
 )
