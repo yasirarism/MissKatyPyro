9e8e03979e49a7fbeacdd56c41415da7684e2c16
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-28 08:03:26 +0700

    Update scrapwebsite.py

--- bot/plugins/scrapwebsite.py
+++ bot/plugins/scrapwebsite.py
@@ -32,3 +32,21 @@ async def melongmovie(_, message):
         return await message.reply(res)
      except IndexError:
         await message.reply("Masukkan kata kunci film yang dicari")
+
+@app.on_message(filters.command(["melongmovie","melongmovie@MissKatyRoBot"], COMMAND_HANDLER))
+async def melong_scrap(_, message):
+    try:
+      judul = message.text.split(" ", maxsplit=1)[1]
+      headers = {
+          'User-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+      }
+
+      html = requests.get(judul, headers=headers)
+      soup = BeautifulSoup(html.text, 'lxml')
+      for ep in soup.findAll(text=re.compile(r"(?i)episode\s+\d+|LINK DOWNLOAD")):
+          hardsub = ep.findPrevious("div")
+          softsub = ep.findNext("div")
+          rep = f"{hardsub}\n{softsub}"
+          await message.reply(rep)
+    except IndexError:
+       await message.reply("Gunakan command /melong <b>[link]</b> untuk scrap link download")
