5cb2dca021769c7f885dad6fa374091d354e097e
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-01-03 07:26:35 +0700

    Update filter_request.py

--- bot/plugins/filter_request.py
+++ bot/plugins/filter_request.py
@@ -4,6 +4,7 @@ import datetime
 from pyrogram import filters, Client
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from bot.utils import time_gap
 
 chat = [-1001128045651, -1001255283935, -1001455886928]
 REQUEST_DB = {}
@@ -13,22 +14,13 @@ async def start(_, message):
     await message.reply_text(
         text=f"Wa'alaikumsalam {message.from_user.mention} 😇"
 )
-    
-@Client.on_message(filters.regex(r"makasi|thank|terimakasih|terima kasih|mksh", re.I) & filters.chat(chat) & ~filters.edited)
-async def start(_, message):
-    pesan = [f"Sama-sama {message.from_user.first_name}",
-             f"You're Welcome {message.from_user.first_name}",
-             "Oke..",
-             "Terimakasih Kembali..",
-             "Sami-Sami...",
-             "Sama-sama, senang bisa membantu..",
-             f"Yups, Sama-sama {message.from_user.first_name}",
-             "Okayyy..."
-            ]
-    await message.reply_text(text=random.choice(pesan))
 
 @Client.on_message(filters.regex(r"#request|#req", re.I) & (filters.text | filters.photo) & filters.chat(-1001255283935) & ~filters.edited)
 async def request_user(client, message):
+    is_in_gap, sleep_time = check_time_gap(message.from_user.id)
+    if is_in_gap:
+        await message.reply("🙄")
+        return
     markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="💬 Lihat Pesan", url=f"https://t.me/c/1255283935/{message.message_id}")],
                                    [InlineKeyboardButton(text="🚫 Tolak", callback_data=f"rejectreq_{message.message_id}_{message.chat.id}"),InlineKeyboardButton(text="✅ Done", callback_data=f"donereq_{message.message_id}_{message.chat.id}")],
                                    [InlineKeyboardButton(text="⚠️ Tidak Tersedia", callback_data=f"unavailablereq_{message.message_id}_{message.chat.id}")],
@@ -51,15 +43,34 @@ async def request_user(client, message):
       await message.reply_text(text=f"Hai {message.from_user.mention}, request kamu sudah dikirim yaa. Harap bersabar mungkin admin juga punya kesibukan lain.\n\n<b>Sisa Request:</b> {3 - REQUEST_DB[user_id]}x", quote=True, reply_markup=markup2)
     except:
       pass
+
 def clear_reqdict():
     REQUEST_DB.clear()
 
+    
+@Client.on_message(filters.regex(r"makasi|thank|terimakasih|terima kasih|mksh", re.I) & filters.chat(chat) & ~filters.edited)
+async def start(_, message):
+    pesan = [f"Sama-sama {message.from_user.first_name}",
+             f"You're Welcome {message.from_user.first_name}",
+             "Oke..",
+             "Yoi..",
+             "Terimakasih Kembali..",
+             "Sami-Sami...",
+             "Sama-sama, senang bisa membantu..",
+             f"Yups, Sama-sama {message.from_user.first_name}",
+             "Okayyy..."
+            ]
+    await message.reply_text(text=random.choice(pesan))
+
 @Client.on_callback_query(filters.regex(r"^donereq"))
 async def _callbackreq(c: Client, q: CallbackQuery):
-    user = await c.get_chat_member(-1001404537486, q.from_user.id)
+    try:
+      user = await c.get_chat_member(-1001404537486, q.from_user.id)
+    except:
+      pass
     if user.status in ['administrator','creator']:
        i, msg_id, chat_id = q.data.split('_')
-       await c.send_message(chat_id=chat_id, text=f"#Done\nDone ✅, pastikan join channel dan grup yaahh untuk melihat request-an nya.", reply_to_message_id=int(msg_id))
+       await c.send_message(chat_id=chat_id, text=f"#Done\nDone ✅, Selamat menonton...", reply_to_message_id=int(msg_id))
        if q.message.caption:
           await q.message.edit_text(f"<b>COMPLETED</b>\n\n<s>{q.message.caption}</s>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="✅ Request Completed", callback_data="reqcompl")]]))
        else:
@@ -70,10 +81,13 @@ async def _callbackreq(c: Client, q: CallbackQuery):
     
 @Client.on_callback_query(filters.regex(r"^dahada"))
 async def _callbackreqada(c: Client, q: CallbackQuery):
-    user = await c.get_chat_member(-1001404537486, q.from_user.id)
+    try:
+       user = await c.get_chat_member(-1001404537486, q.from_user.id)
+    except:
+       pass
     if user.status in ['administrator','creator']:
        i, msg_id, chat_id = q.data.split('_')
-       await c.send_message(chat_id=chat_id, text=f"#AlreadyAvailable\nFilm/series yang direquest sudah ada sebelumnya. Selamat mencari..", reply_to_message_id=int(msg_id))
+       await c.send_message(chat_id=chat_id, text=f"#Film/series yang direquest sudah ada sebelumnya. Biasakan mencari terlebih dahulu..", reply_to_message_id=int(msg_id))
        if q.message.caption:
           await q.message.edit_text(f"<b>#AlreadyAvailable</b>\n\n<s>{q.message.caption}</s>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🔍 Request Sudah Ada", callback_data="reqavailable")]]))
        else:
@@ -84,10 +98,13 @@ async def _callbackreqada(c: Client, q: CallbackQuery):
 
 @Client.on_callback_query(filters.regex(r"^rejectreq"))
 async def _callbackreject(c: Client, q: CallbackQuery):
-    user = await c.get_chat_member(-1001404537486, q.from_user.id)
+    try:
+       user = await c.get_chat_member(-1001404537486, q.from_user.id)
+    except:
+       pass
     if user.status in ['administrator','creator']:
        i, msg_id, chat_id = q.data.split('_')
-       await c.send_message(chat_id=chat_id, text=f"#Rejected\nMohon maaf, request kamu ditolak karena tidak sesuai rules. Harap baca rules nya dulu yaa 🙃.", reply_to_message_id=int(msg_id))
+       await c.send_message(chat_id=chat_id, text=f"Mohon maaf, request kamu ditolak karena tidak sesuai rules. Harap baca rules nya no.6 yaa 🙃.", reply_to_message_id=int(msg_id))
        if q.message.caption:
           await q.message.edit_text(f"<b>REJECTED</b>\n\n<s>{q.message.caption}</s>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🚫 Request Rejected", callback_data="reqreject")]]))
        else:
@@ -98,15 +115,18 @@ async def _callbackreject(c: Client, q: CallbackQuery):
 
 @Client.on_callback_query(filters.regex(r"^unavailablereq"))
 async def _callbackunav(c: Client, q: CallbackQuery):
-    user = await c.get_chat_member(-1001404537486, q.from_user.id)
+    try:
+       user = await c.get_chat_member(-1001404537486, q.from_user.id)
+    except:
+       pass
     if user.status in ['administrator','creator']:
        i, msg_id, chat_id = q.data.split('_')
-       await c.send_message(chat_id=chat_id, text=f"#Unavailable\nMohon maaf, request kamu tidak tersedia 😕..", reply_to_message_id=int(msg_id))
+       await c.send_message(chat_id=chat_id, text=f"Mohon maaf, request kamu tidak tersedia, mungkin belum rilis atau memang ga ada versi digital..", reply_to_message_id=int(msg_id))
        if q.message.caption:
           await q.message.edit_text(f"<b>UNAVAILABLE</b>\n\n<s>{q.message.caption}</s>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⚠️ Request Unavailable", callback_data="requnav")]]))
        else:
           await q.message.edit_text(f"<b>UNAVAILABLE</b>\n\n<s>{q.message.text}</s>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⚠️ Request Unavailable", callback_data="requnav")]]))
-       await q.answer("Request tidak tersedia.")
+       await q.answer("Request tidak tersedia, mungkin belum rilis atau memang tidak tersedia versi digital.")
     else:
        await q.answer("Apa motivasi kamu menekan tombol ini?", show_alert=True)
         
@@ -125,7 +145,7 @@ async def _callbackaft_unav(c: Client, q: CallbackQuery):
 
 @Client.on_callback_query(filters.regex(r"^reqavailable$"))
 async def _callbackaft_dahada(c: Client, q: CallbackQuery):
-      await q.answer("Request ini sudah ada sebelumnya, silahkan cari 🔍 di channel atau grup yaa, selamat mencari..", show_alert=True)
+      await q.answer("Request ini sudah ada sebelumnya, silahkan cari 🔍 di channel atau grup yaa..", show_alert=True)
 
 scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
 scheduler.add_job(clear_reqdict, trigger="cron", hour=7, minute=0)
