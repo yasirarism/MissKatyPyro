282e7484a7a0ee25c44d83bcc63aaa0f62f2f8d4
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-01-09 06:49:39 +0700

    Update nightmode.py

--- bot/plugins/nightmode.py
+++ bot/plugins/nightmode.py
@@ -93,6 +93,6 @@ async def _callbackanightmd(c: Client, q: CallbackQuery):
 scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
 scheduler.add_job(job_close, trigger="cron", hour=22, minute=0)
 scheduler.add_job(job_close_ymoviez, trigger="cron", hour=22, minute=15)
-scheduler.add_job(job_open, trigger="cron", hour=6, minute=45)
+scheduler.add_job(job_open, trigger="cron", hour=6, minute=55)
 scheduler.add_job(job_open_ymoviez, trigger="cron", hour=9, minute=0)
 scheduler.start()
