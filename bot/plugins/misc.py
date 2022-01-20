dec4e0b8ddbe310ee18f05f950a1eb5e28e8108e
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-01-20 12:04:29 +0700

    Update misc.py

--- bot/plugins/misc.py
+++ bot/plugins/misc.py
@@ -299,15 +299,15 @@ async def mdl_callback(bot: Client, query: CallbackQuery):
       try:
         res = requests.get(f"https://kuryana.vercel.app/id/{slug}").json()
         result += f"Title: <a href='{res['data']['link']}'>{res['data']['title']}</a>\n"
-        result += f"AKA: {res['others']['also_known_as']}\n\n"
-        result += f"Rating: {res['details']['score']}\n"
-        result += f"Type: {res['details']['type']}\n"
-        result += f"Country: {res['details']['country']}\n"
-        result += f"Release Date: {res['details']['release_date']}\n"
-        result += f"Duration: {res['details']['duration']}\n"
-        result += f"Genre: {res['others']['genres']}\n"
-        result += f"Synopsis: {res['data']['synopsis']}\n"
-        result += f"Tags: {res['others']['tags']}\n"
+        result += f"AKA: {res['data']['others']['also_known_as']}\n\n"
+        result += f"Rating: {res['data']['details']['score']}\n"
+        result += f"Type: {res['data']['details']['type']}\n"
+        result += f"Country: {res['data']['details']['country']}\n"
+        result += f"Release Date: {res['data']['details']['release_date']}\n"
+        result += f"Duration: {res['data']['details']['duration']}\n"
+        result += f"Genre: {res['data']['others']['genres']}\n"
+        result += f"Synopsis: {res['data']['data']['synopsis']}\n"
+        result += f"Tags: {res['data']['others']['tags']}\n"
         await query.message.edit_text(f"{result}")
       except Exception as e:
         await query.message.edit_text(f"<b>ERROR:</b>\n<code>{e}</code>")
