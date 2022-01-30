579cf8646f4e3081d236519f35b068cc2ca362da
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-01-30 21:23:39 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /YasirBot
 fi
-cd /EvaMaria
+cd /YasirBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m bot
