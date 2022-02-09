da0ed63390e65d540f92027b2a0cbdb1a43544cd
yasir <yasiramunandar@gmail.com>
2022-02-09 22:25:35 +0700

    tes inline search

--- bot/__init__.py
+++ bot/__init__.py
@@ -12,10 +12,18 @@ from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN
 from telethon import TelegramClient
+from Python_ARQ import ARQ
+from aiohttp import ClientSession
 from utils import temp
 
 SESSION_STRING = "BQCWhJPeI7P0GAuDZ9rPoBWx0F-xVztjjYBx2DlfkvE3tdHt8qHd9fxI-kCMm4Lj9do4ad7ZnUc76erdfz3sJQjmg8ptm7tDYBMFvwuiUCwsiiGgaG4Xf80183v8yLHG2bC0qbVo-yrC_aRKK6QFN4RO_ny5M4SlVLnUxbFe7E9Y96WtV-6GO2pw9RxXoMSA5NjN5x__xV9FVqJErmYpJrQMJN9yofxnG74GWQNfu1NoS2CbShnF-ecNTCqTISV-ovHFTmV3dAi3t8EyHp6jrw14N2hhdaTCUyqxSwYD9XHTYOIxlk7N65Riey1t5BcwhJrVxHHlR7lJQZzKzCrBPLMGJM0vaAA"
 botStartTime = time.time()
+
+ARQ_API_URL = "https://thearq.tech/"
+ARQ_API_KEY = "CYQWYJ-OMSAFJ-ARHIAT-SDADAX-ARQ"
+
+aiohttpsession = ClientSession()
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
 app = Client(
     session_name=SESSION,
---
+++ bot/plugins/inline_search.py
@@ -0,0 +1,48 @@
+from bot import app
+from bot.utils.inline_func import *
+
+
+@app.on_inline_query()
+async def inline_query_handler(client, query):
+    try:
+        text = query.query.strip().lower()
+        answers = []
+        if text.split()[0] == "google":
+            if len(text.split()) < 2:
+                return await client.answer_inline_query(
+                    query.id,
+                    results=answers,
+                    switch_pm_text="Google Search | google [QUERY]",
+                    switch_pm_parameter="inline",
+                )
+            tex = text.split(None, 1)[1].strip()
+            answerss = await google_search_func(answers, tex)
+            await client.answer_inline_query(
+                query.id,
+                results=answerss,
+            )
+        elif text.split()[0] == "yt":
+            if len(text.split()) < 2:
+                return await client.answer_inline_query(
+                    query.id,
+                    results=answers,
+                    switch_pm_text="YouTube Search | yt [QUERY]",
+                    switch_pm_parameter="inline",
+                )
+            tex = text.split(None, 1)[1].strip()
+            answerss = await youtube_func(answers, tex)
+            await client.answer_inline_query(query.id, results=answerss)
+
+        elif text.split()[0] == "lirik":
+            if len(text.split()) < 2:
+                return await client.answer_inline_query(
+                    query.id,
+                    results=answers,
+                    switch_pm_text="Lyrics Search | lyrics [QUERY]",
+                    switch_pm_parameter="inline",
+                )
+            tex = text.split(None, 1)[1].strip()
+            answerss = await lyrics_func(answers, tex)
+            await client.answer_inline_query(query.id, results=answerss)
+    except Exception as e:
+        pass
