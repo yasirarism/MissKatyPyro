79b54dda38183b3f956648d1ff14fc6839e77df4
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-11 08:38:09 +0700

    Update decorator.py

--- bot/utils/decorator.py
+++ bot/utils/decorator.py
@@ -39,17 +39,12 @@ def capture_err(func):
             return
         except Exception as err:
             exc = traceback.format_exc()
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            errors = traceback.format_exception(
-                value=exc_obj,
-                tb=exc_tb,
-            )
             error_feedback = split_limits(
                 "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
                     0 if not message.from_user else message.from_user.id,
                     0 if not message.chat else message.chat.id,
                     message.text or message.caption,
-                    "".join(errors),
+                    exc,
                 ),
             )
             for x in error_feedback:
