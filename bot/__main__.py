66ce659bc313f04f4442d91ad05b09ec792b4ff5
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-12 09:44:33 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,5 @@
 import logging
-from bot import app, user, telethon, telethon_ubot
+from bot import app, user
 from info import BOT_TOKEN
 from utils import temp
 from pyrogram.raw.all import layer
@@ -9,14 +9,18 @@ from pyrogram import idle, __version__
 if __name__ == "__main__":
     app.start()
     user.start()
-    telethon.start(bot_token=BOT_TOKEN)
-    telethon_ubot.start()
     me = app.get_me()
     user = user.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
-    app.send_message(617426792, f"USERBOT AND BOT STARTED..\nUserBot: {user.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
+    try:
+      app.send_message(617426792, f"USERBOT AND BOT STARTED..\nUserBot: {user.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
+    except:
+      pass
     logging.info(f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
     logging.info(f"{user.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{user.username}.")
     idle()
+    app.stop()
+    user.stop()
+    logging.info("Userbot and Bot stopped..")
