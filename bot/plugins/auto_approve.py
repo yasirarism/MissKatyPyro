5aece6d9d7bf46d861e2b67272381571db77e711
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-16 22:42:20 +0700

    Update auto_approve.py

--- bot/plugins/auto_approve.py
+++ bot/plugins/auto_approve.py
@@ -6,17 +6,19 @@ from pyrogram.types import ChatJoinRequest, InlineKeyboardButton, InlineKeyboard
 async def approve_join_chat(c: Client, m: ChatJoinRequest):
    if not m.from_user:
       return
-   markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="💬 Setuju", callback_data="approve"), InlineKeyboardButton(text="💬 Tidak Setuju", callback_data="declined")]])
+   markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="💬 Setuju", callback_dataf=f"approve_{message.chat.id}"), InlineKeyboardButton(text="💬 Tidak Setuju", callback_data="declined_{message.chat.id}")]])
    await c.send_message(m.from_user.id, "Apakah anda setuju..", reply_markup=markup)
 
 @app.on_callback_query(filters.regex(r"^approve$"))
 async def approve_chat(c: Client, q: CallbackQuery):
+      i, chat = query.data.split('_')
       await q.message.edit(f"Yeayy, selamat kamu bisa bergabung di Channel YMovieZ Reborn. Jangan lupa share yakk biar makin banyak subnya..")
-      await c.approve_chat_join_request(q.message.chat.id, q.from_user.id)
+      await c.approve_chat_join_request(chat, q.from_user.id)
 
 @app.on_callback_query(filters.regex(r"^declined$"))
 async def decline_chat(c: Client, q: CallbackQuery):
+      i, chat = query.data.split('_')
       await q.message.edit("Yahh, sayang banget kamu ga jadi subs ke channel ini..")
-      await c.approve_chat_join_request(q.message.chat.id, q.from_user.id)
+      await c.approve_chat_join_request(chat, q.from_user.id)
 
 # Todo: Add exception if bot blocked
