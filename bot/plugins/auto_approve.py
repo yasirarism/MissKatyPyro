8c33dd9bfde5576ca32088834da9eef3b98c740f
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-16 22:30:45 +0700

    Update auto_approve.py

--- bot/plugins/auto_approve.py
+++ bot/plugins/auto_approve.py
@@ -6,5 +6,16 @@ from pyrogram.types import ChatJoinRequest
 async def approve_join_chat(c: Client, m: ChatJoinRequest):
    if not m.from_user:
       return
-   await c.send_message(m.from_user.id, "Makasih udah join..")
-   await c.approve_chat_join_request(m.chat.id, m.from_user.id)
+   markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="💬 Setuju", callback_data=approve), InlineKeyboardButton(text="💬 Tidak Setuju", callback_data=declined)]])
+   await c.send_message(m.from_user.id, "Apakah anda setuju..", reply_markup=markup)
+   
+
+@app.on_callback_query(filters.regex(r"^approve$"))
+async def _callbackanightmd(c: Client, q: CallbackQuery):
+      await q.message.edit(f"Yeayy, selamat kamu bisa bergabung di Channel YMovieZ Reborn. Jangan lupa share yakk biar makin banyak subnya..")
+      await c.approve_chat_join_request(q.chat.id, q.from_user.id)
+
+@app.on_callback_query(filters.regex(r"^declined$"))
+async def _callbackanightmd(c: Client, q: CallbackQuery):
+      await q.message.edit("Yahh, sayang banget kamu ga jadi subs ke channel ini..:)
+      await c.approve_chat_join_request(q.chat.id, q.from_user.id)
