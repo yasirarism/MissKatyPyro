c9151f63f17f1a07902d81772191a17ae4685c71
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-16 22:04:49 +0700

    Create auto_approve.py

---
+++ bot/plugins/auto_approve.py
@@ -0,0 +1,13 @@
+from bot import app
+from pyrogram import Client, filters
+from pyrogram.types import ChatJoinRequest
+
+@app.on_chat_join_request(filters.chat(-1001686184174))
+async def approve_join_chat(c: Client, m: ChatJoinRequest):
+   if not m.from_user:
+      return
+   try:
+      await m.reply(m.chat.id, "Hai")
+  except FloodWait as e:
+     await asyncio.sleep(e.x + 2)
+     await c.approve_chat_join_request(m.chat.id, m.from_user.id)
