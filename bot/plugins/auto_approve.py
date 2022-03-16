f65ebff1b66f20034a7b15317d7437ac528e4468
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-16 22:13:24 +0700

    Update auto_approve.py

--- bot/plugins/auto_approve.py
+++ bot/plugins/auto_approve.py
@@ -7,6 +7,6 @@ async def approve_join_chat(c: Client, m: ChatJoinRequest):
    if not m.from_user:
       return
    try:
-      await m.reply(m.chat.id, "Hai")
+      await c.send_message(m.chat.id, "Hai")
    except Exception as e:
       await c.approve_chat_join_request(m.chat.id, m.from_user.id)
