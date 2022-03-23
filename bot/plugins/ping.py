e637fedf0937621f610103a01db0085595b7e8e2
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-23 13:44:58 +0700

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -1,12 +1,14 @@
 import time
 from pyrogram import filters
 from info import COMMAND_HANDLER
-from bot import app, user
+from bot import app, user, botStartTime
+from bot.utils.human_read import get_readable_time
 
 @app.on_message(filters.command("ping", COMMAND_HANDLER))
 async def ping(_, message):
+    currentTime = get_readable_time(time.time() - botStartTime)
     start_t = time.time()
     rm = await message.reply_text("🐱 Pong!!...")
     end_t = time.time()
     time_taken_s = round(end_t - start_t, 3)
-    await rm.edit(f"<b>🐈 Pong!</b>\n<code>{time_taken_s} detik</code>\n\n<b>Userbot:</b> <a href='https://t.me/{(await user.get_me()).username}'>{(await user.get_me()).first_name}</a>\n<b>Bot:</b> @{(await app.get_me()).username}", disable_web_page_preview=True)
+    await rm.edit(f"<b>🐈 Pong!</b>\n<code>{time_taken_s} detik</code>\n\n<b>Userbot:</b> <a href='https://t.me/{(await user.get_me()).username}'>{(await user.get_me()).first_name}</a>\n<b>Bot:</b> @{(await app.get_me()).username}\n<b>Uptime:</b> <code>{currentTime}</code>", disable_web_page_preview=True)
