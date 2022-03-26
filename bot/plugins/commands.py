2327e26727f450c9f7745c4c0e706d8dfab00465
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-26 18:23:11 +0700

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -152,11 +152,11 @@ async def channel_info(bot, message):
         os.remove(file)
 
 
-@Client.on_message(filters.command('logs') & filters.user(ADMINS))
+@Client.on_message(filters.command(['logs']) & filters.user(ADMINS))
 async def log_file(bot, message):
     """Send log file"""
     try:
-        await message.reply_document('TelegramBot.log')
+        await message.reply_document('MissKatyLogs.txt')
     except Exception as e:
         await message.reply(str(e))
 
