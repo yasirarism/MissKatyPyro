bccb69e224bc4c32b915562626e7f614f6b200b1
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-26 19:46:25 +0700

    Update restart.py

--- bot/plugins/restart.py
+++ bot/plugins/restart.py
@@ -5,8 +5,8 @@ from pyrogram import filters
 from info import ADMINS, COMMAND_HANDLER
 
 @app.on_message(filters.command(["restart","restart@MissKatyRoBot"], COMMAND_HANDLER) & filters.user(ADMINS))
-async def restart(client, message):
-    await message.reply("Restarting, Please wait!")
+def restart(client, message):
+    message.reply("Restarting, Please wait!")
     app.stop()
     user.stop()
     # os.execl(executable, executable, "-m", "bot")
