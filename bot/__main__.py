b5edec04aa1bf126e5816119abcffe3033c4efe4
yasir <yasiramunandar@gmail.com>
2022-04-25 11:16:44 +0700

    fix init

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,11 +4,8 @@ api_id = 1046625
 api_hash = "c68afc924b92d73ce27708b155f1e5b4"
 bot_token = "1507530289:AAFLdrEV-SmWiQPyfmMe9r2Y-LmtgB0Shdw"
 
-app = Client("YasirUBot", api_id=api_id, api_hash=api_hash)
-app2 = Client("MissKatyBot",
-              api_id=api_id,
-              api_hash=api_hash,
-              bot_token=bot_token)
+app = Client("YasirUBot")
+app2 = Client("MissKatyBot")
 
 
 @app.on_message(filters.command("hai"))
