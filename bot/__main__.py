b56da596489d4e3a5fb6d40a89104a6a901923d0
yasir <yasiramunandar@gmail.com>
2022-04-25 09:44:40 +0700

    tes update pyrogram

--- bot/__main__.py
+++ bot/__main__.py
@@ -6,28 +6,28 @@ from utils import temp
 from pyrogram.raw.all import layer
 from pyrogram import idle, __version__, compose
 
+
 # Run Bot
-#async def main():
-#    await compose([app, user])
-
-# asyncio.run(main())
-
-import asyncio
-from pyrogram import Client, idle
-
-
 async def main():
-    apps = [Client("MissKatyBot.session"), Client("YasirUbot.session")]
-
-    ...  # Set up handlers
-
-    for app in apps:
-        await app.start()
-
-    await idle()
-
-    for app in apps:
-        await app.stop()
+    await compose([app, user])
 
 
 asyncio.run(main())
+me = app.get_me()
+user = user.get_me()
+temp.ME = me.id
+temp.U_NAME = me.username
+temp.B_NAME = me.first_name
+try:
+    app.send_message(
+        617426792,
+        f"USERBOT AND BOT STARTED..\nUserBot: {user.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
+    )
+except:
+    pass
+logging.info(
+    f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
+)
+logging.info(
+    f"{user.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{user.username}."
+)
