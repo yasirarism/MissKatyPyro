5ea485643928cd4c03cd03c1a3af89e51ce5b951
yasir <yasiramunandar@gmail.com>
2022-04-25 15:17:44 +0700

    tes

--- bot/__main__.py
+++ bot/__main__.py
@@ -8,26 +8,7 @@ from pyrogram import idle, __version__
 # Run Bot
 if __name__ == "__main__":
     app.start()
-    user.start()
-    me = app.get_me()
-    user = user.get_me()
-    temp.ME = me.id
-    temp.U_NAME = me.username
-    temp.B_NAME = me.first_name
-    try:
-        app.send_message(
-            617426792,
-            f"USERBOT AND BOT STARTED..\nUserBot: {user.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
-        )
-    except:
-        pass
-    logging.info(
-        f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
-    )
-    logging.info(
-        f"{user.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{user.username}."
-    )
+    # user.start()
     idle()
     app.stop()
-    user.stop()
-    logging.info("Userbot and Bot stopped..")
+    # user.stop()
