d7a73b0912cbcb273dcfb9270afe9a25691819c2
yasir <yasiramunandar@gmail.com>
2022-04-25 16:59:44 +0700

    tes

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,14 +1,30 @@
 import logging
+import asyncio
 from bot import app, user
 from info import BOT_TOKEN
 from utils import temp
 from pyrogram.raw.all import layer
-from pyrogram import idle, __version__
+from pyrogram import idle, __version__, compose, Client
+
 
 # Run Bot
-if __name__ == "__main__":
-    app.start()
-    user.start()
-    idle()
-    app.stop()
-    user.stop()
+async def main():
+    await compose([app, user])
+    me = await app.get_me()
+    user = await user.get_me()
+    temp.ME = me.id
+    temp.U_NAME = me.username
+    temp.B_NAME = me.first_name
+    try:
+        app.send_message(
+            617426792,
+            f"USERBOT AND BOT STARTED with Pyrogram v{__version__}..\nUserBot: {user.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
+        )
+    except:
+        pass
+    logging.info(
+        f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
+    )
+
+
+asyncio.run(main())
