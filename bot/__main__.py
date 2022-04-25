551cc836fb5319753bd28635b10dcb342daa0bc5
yasir <yasiramunandar@gmail.com>
2022-04-25 17:04:38 +0700

    tes

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,21 +10,21 @@ from pyrogram import idle, __version__, compose, Client
 # Run Bot
 async def main():
     await compose([app, user])
-    me = await app.get_me()
-    ubot = await user.get_me()
-    temp.ME = me.id
-    temp.U_NAME = me.username
-    temp.B_NAME = me.first_name
-    try:
-        app.send_message(
-            617426792,
-            f"USERBOT AND BOT STARTED with Pyrogram v{__version__}..\nUserBot: {ubot.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
-        )
-    except:
-        pass
-    logging.info(
-        f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
-    )
 
 
 asyncio.run(main())
+me = app.get_me()
+ubot = user.get_me()
+temp.ME = me.id
+temp.U_NAME = me.username
+temp.B_NAME = me.first_name
+try:
+    app.send_message(
+        617426792,
+        f"USERBOT AND BOT STARTED..\nUserBot: {ubot.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
+    )
+except:
+    pass
+logging.info(
+    f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
+)
