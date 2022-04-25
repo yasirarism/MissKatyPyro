e07bd405a943d92a353a1e522380f41e49cb352d
yasir <yasiramunandar@gmail.com>
2022-04-25 15:32:48 +0700

    tes

--- bot/plugins/p_ttishow.py
+++ bot/plugins/p_ttishow.py
@@ -11,8 +11,7 @@ from Script import script
 from pyrogram.errors import ChatAdminRequired
 
 
-@Client.on_message(f
-ilters.new_chat_members & filters.group)
+@Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
     r_j_check = [u.id for u in message.new_chat_members]
     if temp.ME in r_j_check:
