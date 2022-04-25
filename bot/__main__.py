af83b1671b4206ae967377e4cd3523419fa9a947
yasir <yasiramunandar@gmail.com>
2022-04-25 08:58:36 +0700

    tes update pyrogram

--- bot/__main__.py
+++ bot/__main__.py
@@ -6,9 +6,11 @@ from utils import temp
 from pyrogram.raw.all import layer
 from pyrogram import idle, __version__, compose
 
+
 # Run Bot
 async def main():
-    await compose([app, user])
+    await compose([app])
+
 
 asyncio.run(main())
 me = app.get_me()
@@ -17,8 +19,15 @@ temp.ME = me.id
 temp.U_NAME = me.username
 temp.B_NAME = me.first_name
 try:
-  app.send_message(617426792, f"USERBOT AND BOT STARTED..\nUserBot: {user.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
+    app.send_message(
+        617426792,
+        f"USERBOT AND BOT STARTED..\nUserBot: {user.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
+    )
 except:
-  pass
-logging.info(f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
-logging.info(f"{user.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{user.username}.")
+    pass
+logging.info(
+    f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
+)
+logging.info(
+    f"{user.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{user.username}."
+)
