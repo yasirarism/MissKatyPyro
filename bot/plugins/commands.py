e49c8858455f699fc0d9168f2463be9a7e1ef64a
yasir <yasiramunandar@gmail.com>
2022-04-25 15:37:04 +0700

    tes

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -6,7 +6,7 @@ import asyncio
 from Script import script
 import shutil, psutil
 
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from bot import botStartTime
 from bot.utils.human_read import get_readable_time, get_readable_file_size
 from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
@@ -16,26 +16,38 @@ from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PICS, COMMAND_HANDLER
 from utils import get_size, is_subscribed, temp
 import re
+
 logger = logging.getLogger(__name__)
 
-@Client.on_message(filters.command(["help","help@MissKatyRoBot"], COMMAND_HANDLER))
+
+@Client.on_message(
+    filters.command(["help", "help@MissKatyRoBot"], COMMAND_HANDLER))
 async def help(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
-            [InlineKeyboardButton(text="ℹ️ Klik Saya", url=f"https://t.me/{temp.U_NAME}?start=help")],
+            [
+                InlineKeyboardButton(
+                    text="ℹ️ Klik Saya",
+                    url=f"https://t.me/{temp.U_NAME}?start=help")
+            ],
         ]
-        return await message.reply("Silahkan PM saya untuk melihat menu bantuan..", reply_markup=InlineKeyboardMarkup(buttons))
+        return await message.reply(
+            "Silahkan PM saya untuk melihat menu bantuan..",
+            reply_markup=InlineKeyboardMarkup(buttons))
     else:
         buttons = [[
             InlineKeyboardButton('Admin', callback_data='manuelfilter'),
             InlineKeyboardButton('Code Runner', callback_data='autofilter')
-            ],[
-            InlineKeyboardButton('Connection', callback_data='coct'),
-            InlineKeyboardButton('Misc', callback_data='extra')
-            ],[
-            InlineKeyboardButton('🏠 Home', callback_data='start'),
-            InlineKeyboardButton('🔮 Status', callback_data='stats')
-        ]]
+        ],
+                   [
+                       InlineKeyboardButton('Connection',
+                                            callback_data='coct'),
+                       InlineKeyboardButton('Misc', callback_data='extra')
+                   ],
+                   [
+                       InlineKeyboardButton('🏠 Home', callback_data='start'),
+                       InlineKeyboardButton('🔮 Status', callback_data='stats')
+                   ]]
         currentTime = get_readable_time(time.time() - botStartTime)
         total, used, free = shutil.disk_usage('.')
         total = get_readable_file_size(total)
@@ -45,25 +57,29 @@ async def help(client, message):
         memory = psutil.virtual_memory().percent
         disk = psutil.disk_usage('/').percent
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(
-            text=script.HELP_TXT.format(message.from_user.mention, currentTime, total, free, used, cpuUsage, memory, disk),
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
-                                    
-@Client.on_message(filters.command(["start","start@MissKatyRoBot"], COMMAND_HANDLER))
+        await message.reply_text(text=script.HELP_TXT.format(
+            message.from_user.mention, currentTime, total, free, used,
+            cpuUsage, memory, disk),
+                                 reply_markup=reply_markup,
+                                 parse_mode=enums.ParseMode.HTML)
+
+
+@Client.on_message(
+    filters.command(["start", "start@MissKatyRoBot"], COMMAND_HANDLER))
 async def start(client, message):
     if message.chat.type == 'private' and message.text == '/start help':
         buttons = [[
             InlineKeyboardButton('Admin', callback_data='manuelfilter'),
             InlineKeyboardButton('Code Runner', callback_data='autofilter')
-            ],[
-            InlineKeyboardButton('Stickers', callback_data='coct'),
-            InlineKeyboardButton('Misc', callback_data='extra')
-            ],[
-            InlineKeyboardButton('🏠 Home', callback_data='start'),
-            InlineKeyboardButton('🔮 Status', callback_data='stats')
-        ]]
+        ],
+                   [
+                       InlineKeyboardButton('Stickers', callback_data='coct'),
+                       InlineKeyboardButton('Misc', callback_data='extra')
+                   ],
+                   [
+                       InlineKeyboardButton('🏠 Home', callback_data='start'),
+                       InlineKeyboardButton('🔮 Status', callback_data='stats')
+                   ]]
         currentTime = get_readable_time(time.time() - botStartTime)
         total, used, free = shutil.disk_usage('.')
         total = get_readable_file_size(total)
@@ -73,57 +89,79 @@ async def start(client, message):
         memory = psutil.virtual_memory().percent
         disk = psutil.disk_usage('/').percent
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(
-            text=script.HELP_TXT.format(message.from_user.mention, currentTime, total, free, used, cpuUsage, memory, disk),
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
+        await message.reply_text(text=script.HELP_TXT.format(
+            message.from_user.mention, currentTime, total, free, used,
+            cpuUsage, memory, disk),
+                                 reply_markup=reply_markup,
+                                 parse_mode=enums.ParseMode.HTML)
         return
     if message.chat.type in ['group', 'supergroup']:
-        buttons = [
-            [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/YasirPediaChannel')
-            ],
-            [
-                InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            ]
-            ]
+        buttons = [[
+            InlineKeyboardButton('🤖 Updates',
+                                 url='https://t.me/YasirPediaChannel')
+        ],
+                   [
+                       InlineKeyboardButton(
+                           'ℹ️ Help',
+                           url=f"https://t.me/{temp.U_NAME}?start=help"),
+                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
-        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        await message.reply(script.START_TXT.format(
+            message.from_user.mention if message.from_user else
+            message.chat.title, temp.U_NAME, temp.B_NAME),
+                            reply_markup=reply_markup,
+                            disable_web_page_preview=True)
+        await asyncio.sleep(
+            2
+        )  # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
-            total=await client.get_chat_members_count(message.chat.id)
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
+            total = await client.get_chat_members_count(message.chat.id)
+            await client.send_message(
+                LOG_CHANNEL,
+                script.LOG_TEXT_G.format(message.chat.title, message.chat.id,
+                                         total, "Unknown"))
             await db.add_chat(message.chat.id, message.chat.title)
-        return 
+        return
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
-        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+        await client.send_message(
+            LOG_CHANNEL,
+            script.LOG_TEXT_P.format(message.from_user.id,
+                                     message.from_user.mention))
     if message.text == '/start':
-        buttons = [[
-            InlineKeyboardButton('➕ Tambahkan Saya ke Grup ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('YMovieZNew Channel', url='https://t.me/YMovieZNew'),
-            InlineKeyboardButton('Updates', url='https://t.me/YasirPediaChannel')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
-            ],[
-            InlineKeyboardButton('📦 Source Code', url='tg://need_update_for_some_feature')
-        ]]
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    '➕ Tambahkan Saya ke Grup ➕',
+                    url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ],
+            [
+                InlineKeyboardButton('YMovieZNew Channel',
+                                     url='https://t.me/YMovieZNew'),
+                InlineKeyboardButton('Updates',
+                                     url='https://t.me/YasirPediaChannel')
+            ],
+            [
+                InlineKeyboardButton('ℹ️ Help', callback_data='help'),
+                InlineKeyboardButton('😊 About', callback_data='about')
+            ],
+            [
+                InlineKeyboardButton('📦 Source Code',
+                                     url='tg://need_update_for_some_feature')
+            ]
+        ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
+        await message.reply_photo(photo=random.choice(PICS),
+                                  caption=script.START_TXT.format(
+                                      message.from_user.mention, temp.U_NAME,
+                                      temp.B_NAME),
+                                  reply_markup=reply_markup,
+                                  parse_mode=enums.ParseMode.HTML)
         return
-                    
+
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
-           
     """Send basic information of channel"""
     if isinstance(CHANNELS, (int, str)):
         channels = [CHANNELS]
@@ -160,6 +198,7 @@ async def log_file(bot, message):
     except Exception as e:
         await message.reply(str(e))
 
+
 @Client.on_message(filters.command('delete') & filters.user(ADMINS))
 async def delete(bot, message):
     """Delete file from database"""
@@ -167,7 +206,8 @@ async def delete(bot, message):
     if reply and reply.media:
         msg = await message.reply("Processing...⏳", quote=True)
     else:
-        await message.reply('Reply to file with /delete which you want to delete', quote=True)
+        await message.reply(
+            'Reply to file with /delete which you want to delete', quote=True)
         return
 
     for file_type in ("document", "video", "audio"):
@@ -177,7 +217,7 @@ async def delete(bot, message):
     else:
         await msg.edit('This is not supported file format')
         return
-    
+
     file_id, file_ref = unpack_new_file_id(media.file_id)
 
     result = await Media.collection.delete_one({
@@ -191,16 +231,19 @@ async def delete(bot, message):
             'file_name': file_name,
             'file_size': media.file_size,
             'mime_type': media.mime_type
-            })
+        })
         if result.deleted_count:
             await msg.edit('File is successfully deleted from database')
         else:
-            # files indexed before https://github.com/EvamariaTG/EvaMaria/commit/f3d2a1bcb155faf44178e5d7a685a1b533e714bf#diff-86b613edf1748372103e94cacff3b578b36b698ef9c16817bb98fe9ef22fb669R39 
+            # files indexed before https://github.com/EvamariaTG/EvaMaria/commit/f3d2a1bcb155faf44178e5d7a685a1b533e714bf#diff-86b613edf1748372103e94cacff3b578b36b698ef9c16817bb98fe9ef22fb669R39
             # have original file name.
             result = await Media.collection.delete_one({
-                'file_name': media.file_name,
-                'file_size': media.file_size,
-                'mime_type': media.mime_type
+                'file_name':
+                media.file_name,
+                'file_size':
+                media.file_size,
+                'mime_type':
+                media.mime_type
             })
             if result.deleted_count:
                 await msg.edit('File is successfully deleted from database')
@@ -212,20 +255,12 @@ async def delete(bot, message):
 async def delete_all_index(bot, message):
     await message.reply_text(
         'This will delete all indexed files.\nDo you want to continue??',
-        reply_markup=InlineKeyboardMarkup(
+        reply_markup=InlineKeyboardMarkup([
             [
-                [
-                    InlineKeyboardButton(
-                        text="YES", callback_data="autofilter_delete"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="CANCEL", callback_data="close_data"
-                    )
-                ],
-            ]
-        ),
+                InlineKeyboardButton(text="YES",
+                                     callback_data="autofilter_delete")
+            ],
+            [InlineKeyboardButton(text="CANCEL", callback_data="close_data")],
+        ]),
         quote=True,
     )
-
