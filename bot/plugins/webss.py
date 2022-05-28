29d6b3a8ccdf80c1dc5b23ab07c67eb8610973e5
yasir <yasiramunandar@gmail.com>
2022-05-28 12:28:01 +0700

    Fix webss

--- bot/plugins/webss.py
+++ bot/plugins/webss.py
@@ -1,32 +1,95 @@
-import os
-import asyncio
+"""
+MIT License
+Copyright (c) 2021 TheHamkerCat
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from asyncio import gather
+from base64 import b64decode
+from io import BytesIO
+
 from pyrogram import filters
-from bot import app
-from info import COMMAND_HANDLER
+from pyrogram.types import Message
+
+from .. import app
+from ...info import COMMAND_HANDLER
+from ..utils.decorator import capture_err
+from ..utils.http import post
 
 
-# Broken
-# @app.on_message(filters.command(["webss","webss@MissKatyRoBot"], COMMAND_HANDLER))
+async def take_screenshot(url: str, full: bool = False):
+    url = "https://" + url if not url.startswith("http") else url
+    payload = {
+        "url": url,
+        "width": 1920,
+        "height": 1080,
+        "scale": 1,
+        "format": "jpeg",
+    }
+    if full:
+        payload["full"] = True
+    data = await post(
+        "https://webscreenshot.vercel.app/api",
+        data=payload,
+    )
+    if "image" not in data:
+        return None
+    b = data["image"].replace("data:image/jpeg;base64,", "")
+    file = BytesIO(b64decode(b))
+    file.name = "webss.jpg"
+    return file
+
+
+@app.on_message(filters.command("webss"), COMMAND_HANDLER)
+@capture_err
 async def take_ss(_, message):
+    if len(message.command) < 2:
+        return await eor(message, text="Give A Url To Fetch Screenshot.")
+
+    if len(message.command) == 2:
+        url = message.text.split(None, 1)[1]
+        full = False
+    elif len(message.command) == 3:
+        url = message.text.split(None, 2)[1]
+        full = message.text.split(None, 2)[2].lower().strip() in [
+            "yes",
+            "y",
+            "1",
+            "true",
+        ]
+    else:
+        return await message.reply("Invalid Command.")
+
+    m = await message.reply("Capturing screenshot...")
+
     try:
-        if len(message.command) != 2 or message.reply_to_message:
-            return await message.reply_text(
-                "Gunakan format /webss [URL] untuk mengambil screenshot web")
-        link = message.text.split(None, 1)[1]
-        download_file_path = os.path.join("/downloads",
-                                          f"webss_{message.from_user.id}.jpg")
-        url = f"http://public-restapi.herokuapp.com/api/web-page-screenshot?url={link}&width=1280&height=720"
-        m = await message.reply_text("**Mengambil Screenshot..**")
-        await asyncio.sleep(1)
-        try:
-            await m.edit("**Mengupload gambar..**")
-            await app.send_chat_action(message.chat.id, "upload_photo")
-            await message.reply_photo(
-                photo=url,
-                caption=f"<code>webss_{message.from_user.id}</code>",
-                quote=True)
-        except TypeError:
-            return await m.edit("No Such Website.")
+        photo = await take_screenshot(url, full)
+        if not photo:
+            return await m.edit("Failed To Take Screenshot")
+
+        m = await m.edit("Uploading...")
+
+        if not full:
+            # Full size images have problem with reply_photo, that's why
+            # we need to only use reply_photo if we're not using full size
+            await gather(
+                *[message.reply_document(photo),
+                  message.reply_photo(photo)])
+        else:
+            await message.reply_document(photo)
         await m.delete()
     except Exception as e:
-        await message.reply_text(str(e))
+        await m.edit(str(e))
