abc08bbe48adce37228dcdb5a4a40e2ce44a981c
yasir <yasiramunandar@gmail.com>
2022-06-01 20:46:10 +0700

    Add auto forwarder, thanks to @AbirHasan

---
+++ bot/plugins/auto_forwarder.py
@@ -0,0 +1,96 @@
+# Code copy from https://github.com/AbirHasan2005/Forward-Client
+from bot import user
+from os import environ
+from pyrogram import filters
+from asyncio import sleep
+from pyrogram.types import Message
+from pyrogram.errors import FloodWait
+from info import FORWARD_FILTERS, BLOCK_FILES_WITHOUT_EXTENSIONS, BLOCKED_EXTENSIONS, FORWARD_TO_CHAT_ID, MINIMUM_FILE_SIZE
+
+
+async def FilterMessage(message: Message):
+    if (message.forward_from
+            or message.forward_from_chat) and ("forwarded"
+                                               not in FORWARD_FILTERS):
+        return 400
+    if (len(FORWARD_FILTERS)
+            == 9) or ((message.video and ("video" in FORWARD_FILTERS)) or
+                      (message.document and ("document" in FORWARD_FILTERS)) or
+                      (message.photo and ("photo" in FORWARD_FILTERS)) or
+                      (message.audio and ("audio" in FORWARD_FILTERS)) or
+                      (message.text and ("text" in FORWARD_FILTERS)) or
+                      (message.animation and ("gif" in FORWARD_FILTERS)) or
+                      (message.poll and ("poll" in FORWARD_FILTERS)) or
+                      (message.sticker and ("sticker" in FORWARD_FILTERS))):
+        return 200
+    else:
+        return 400
+
+
+async def CheckBlockedExt(event: Message):
+    media = event.document or event.video or event.audio or event.animation
+    if (BLOCK_FILES_WITHOUT_EXTENSIONS is True) and ("."
+                                                     not in media.file_name):
+        return True
+    if (media is not None) and (media.file_name is not None):
+        _file = media.file_name.rsplit(".", 1)
+        if len(_file) == 2:
+            if (_file[-1].lower()
+                    in BLOCKED_EXTENSIONS) or (_file[-1].upper()
+                                               in BLOCKED_EXTENSIONS):
+                return True
+            else:
+                return False
+        else:
+            return False
+
+
+async def CheckFileSize(msg: Message):
+    media = msg.video or msg.document or msg.audio or msg.photo or msg.animation
+    if (MINIMUM_FILE_SIZE
+            is not None) and (media.file_size < int(MINIMUM_FILE_SIZE)):
+        return False
+    else:
+        return True
+
+
+async def ForwardMessage(client: user, msg: Message):
+    try:
+        ## --- Check 1 --- ##
+        can_forward = await FilterMessage(message=msg)
+        if can_forward == 400:
+            return 400
+        ## --- Check 2 --- ##
+        has_blocked_ext = await CheckBlockedExt(event=msg)
+        if has_blocked_ext is True:
+            return 400
+        ## --- Check 3 --- ##
+        file_size_passed = await CheckFileSize(msg=msg)
+        if file_size_passed is False:
+            return 400
+        ## --- Check 4 --- ##
+        for i in range(len(FORWARD_TO_CHAT_ID)):
+            try:
+                await msg.copy(FORWARD_TO_CHAT_ID[i])
+            except FloodWait as e:
+                await sleep(e.value)
+                await client.send_message(
+                    chat_id="me",
+                    text=f"#FloodWait: Stopped Forwarder for `{e.x}s`!")
+                await sleep(10)
+                await ForwardMessage(client, msg)
+            except Exception as err:
+                await client.send_message(
+                    chat_id="me",
+                    text=
+                    f"#ERROR: `{err}`\n\nUnable to Forward Message to `{str(FORWARD_TO_CHAT_ID[i])}`"
+                )
+    except Exception as err:
+        await client.send_message(chat_id="me", text=f"#ERROR: `{err}`")
+
+
+@user.on_message(filters.text | filters.media)
+async def main(client: user, message: Message):
+    try_forward = await ForwardMessage(client, message)
+    if try_forward == 400:
+        return
--- info.py
+++ info.py
@@ -2,6 +2,8 @@ import re
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
+
+
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
@@ -10,27 +12,40 @@ def is_enabled(value, default):
     else:
         return default
 
+
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
-COMMAND_HANDLER = environ.get('COMMAND_HANDLER','! /').split()
-
+COMMAND_HANDLER = environ.get('COMMAND_HANDLER', '! /').split()
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/90e9a448bc2f8b055b762.jpg https://telegra.ph/file/e412e6ce9ca3ff0f7cef0.jpg https://telegra.ph/file/be3a1ed3f6fecfe6e114d.jpg https://telegra.ph/file/9d0e5c41406e15e1ee50e.jpg https://telegra.ph/file/afeddb3b69f3a4d2b228e.jpg https://telegra.ph/file/5e9097123d74bf37ce253.jpg https://telegra.ph/file/b72773f07bde54db42f8a.jpg https://telegra.ph/file/42760827e2894ca2ca493.jpg')).split()
+PICS = (environ.get(
+    'PICS',
+    'https://telegra.ph/file/90e9a448bc2f8b055b762.jpg https://telegra.ph/file/e412e6ce9ca3ff0f7cef0.jpg https://telegra.ph/file/be3a1ed3f6fecfe6e114d.jpg https://telegra.ph/file/9d0e5c41406e15e1ee50e.jpg https://telegra.ph/file/afeddb3b69f3a4d2b228e.jpg https://telegra.ph/file/5e9097123d74bf37ce253.jpg https://telegra.ph/file/b72773f07bde54db42f8a.jpg https://telegra.ph/file/42760827e2894ca2ca493.jpg'
+)).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [
+    int(admin) if id_pattern.search(admin) else admin
+    for admin in environ.get('ADMINS', '').split()
+]
+CHANNELS = [
+    int(ch) if id_pattern.search(ch) else ch
+    for ch in environ.get('CHANNELS', '0').split()
+]
+auth_users = [
+    int(user) if id_pattern.search(user) else user
+    for user in environ.get('AUTH_USERS', '').split()
+]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+AUTH_CHANNEL = int(
+    auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
@@ -45,8 +60,39 @@ P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
-LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
+IMDB_TEMPLATE = environ.get(
+    "IMDB_TEMPLATE",
+    "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10"
+)
+LONG_IMDB_DESCRIPTION = is_enabled(
+    environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
+
+## Config For AUtoForwarder
+# Forward From Chat ID
+FORWARD_FROM_CHAT_ID = list(
+    set(
+        int(x) for x in environ.get("FORWARD_FROM_CHAT_ID",
+                                    "-1001128045651").split()))
+# Forward To Chat ID
+FORWARD_TO_CHAT_ID = list(
+    set(
+        int(x)
+        for x in environ.get("FORWARD_TO_CHAT_ID", "-1001210537567").split()))
+# Filters for Forwards
+FORWARD_FILTERS = list(
+    set(x for x in environ.get("FORWARD_FILTERS", "video document").split()))
+BLOCKED_EXTENSIONS = list(
+    set(x for x in environ.get(
+        "BLOCKED_EXTENSIONS",
+        "html htm json txt php gif png ink torrent url nfo xml xhtml").split())
+)
+MINIMUM_FILE_SIZE = environ.get("MINIMUM_FILE_SIZE", None)
+BLOCK_FILES_WITHOUT_EXTENSIONS = bool(
+    environ.get("BLOCK_FILES_WITHOUT_EXTENSIONS", True))
+# Forward as Copy. Value Should be True or False
+FORWARD_AS_COPY = bool(environ.get("FORWARD_AS_COPY", True))
+# Sleep Time while Kang
+SLEEP_TIME = int(environ.get("SLEEP_TIME", 10))
