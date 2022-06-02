78f8e1fbb897a65e1e4f1e86a68db54e13a5bbd5
yasir <yasiramunandar@gmail.com>
2022-06-02 11:39:07 +0700

    tes

--- bot/plugins/sed.py
+++ bot/plugins/sed.py
@@ -42,14 +42,19 @@ async def sed(c: app, m: Message):
                         flags=rflags,
                         timeout=1)
     except TimeoutError:
-        await m.reply_text("Oops, your regex pattern has run for too long.")
-    except MessageEmpty:
-        await m.reply_text("Please reply message to use this feature.")
+        return await m.reply_text(
+            "Oops, your regex pattern has run for too long.")
     except regex.error as e:
-        await m.reply_text(str(e))
+        return await m.reply_text(str(e))
     else:
-        await c.send_message(
-            m.chat.id,
-            f"<pre>{html.escape(res)}</pre>",
-            reply_to_message_id=m.reply_to_message.id,
-        )
+        try:
+            await c.send_message(
+                m.chat.id,
+                f"<pre>{html.escape(res)}</pre>",
+                reply_to_message_id=m.reply_to_message.id,
+            )
+        except MessageEmpty:
+            return await m.reply_text(
+                "Please reply message to use this feature.")
+        except Exception as e:
+            return await m.reply_text(f"ERROR: {str(e)}")
