3cf8d4e24c557a370d533373d6a199c30bb49e69
yasir <yasiramunandar@gmail.com>
2022-06-13 15:36:47 +0700

    fix

--- bot/plugins/dev.py
+++ bot/plugins/dev.py
@@ -18,8 +18,8 @@ from subprocess import run as srun
     & filters.user(617426792) & filters.reply)
 async def balas(c, m):
     pesan = m.text.split(' ', 1)
-    await m.reply(pesan[1], reply_to_message_id=m.reply_to_message.id)
     await m.delete()
+    await m.reply(pesan[1], reply_to_message_id=m.reply_to_message.id)
 
 
 @app.on_message(
