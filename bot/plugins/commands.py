d6a29ab3a02d380d662d0bee2576ce36426d7fa1
yasir <yasiramunandar@gmail.com>
2022-06-14 20:43:50 +0700

    Tes AFK

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -11,7 +11,6 @@ from bot import botStartTime
 from bot.utils.human_read import get_readable_time, get_readable_file_size
 from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PICS, COMMAND_HANDLER
 from utils import get_size, is_subscribed, temp
@@ -197,70 +196,3 @@ async def log_file(bot, message):
         await message.reply_document('MissKatyLogs.txt')
     except Exception as e:
         await message.reply(str(e))
-
-
-@Client.on_message(filters.command('delete') & filters.user(ADMINS))
-async def delete(bot, message):
-    """Delete file from database"""
-    reply = message.reply_to_message
-    if reply and reply.media:
-        msg = await message.reply("Processing...⏳", quote=True)
-    else:
-        await message.reply(
-            'Reply to file with /delete which you want to delete', quote=True)
-        return
-
-    for file_type in ("document", "video", "audio"):
-        media = getattr(reply, file_type, None)
-        if media is not None:
-            break
-    else:
-        await msg.edit('This is not supported file format')
-        return
-
-    file_id, file_ref = unpack_new_file_id(media.file_id)
-
-    result = await Media.collection.delete_one({
-        '_id': file_id,
-    })
-    if result.deleted_count:
-        await msg.edit('File is successfully deleted from database')
-    else:
-        file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
-        result = await Media.collection.delete_one({
-            'file_name': file_name,
-            'file_size': media.file_size,
-            'mime_type': media.mime_type
-        })
-        if result.deleted_count:
-            await msg.edit('File is successfully deleted from database')
-        else:
-            # files indexed before https://github.com/EvamariaTG/EvaMaria/commit/f3d2a1bcb155faf44178e5d7a685a1b533e714bf#diff-86b613edf1748372103e94cacff3b578b36b698ef9c16817bb98fe9ef22fb669R39
-            # have original file name.
-            result = await Media.collection.delete_one({
-                'file_name':
-                media.file_name,
-                'file_size':
-                media.file_size,
-                'mime_type':
-                media.mime_type
-            })
-            if result.deleted_count:
-                await msg.edit('File is successfully deleted from database')
-            else:
-                await msg.edit('File not found in database')
-
-
-@Client.on_message(filters.command('deleteall') & filters.user(ADMINS))
-async def delete_all_index(bot, message):
-    await message.reply_text(
-        'This will delete all indexed files.\nDo you want to continue??',
-        reply_markup=InlineKeyboardMarkup([
-            [
-                InlineKeyboardButton(text="YES",
-                                     callback_data="autofilter_delete")
-            ],
-            [InlineKeyboardButton(text="CANCEL", callback_data="close_data")],
-        ]),
-        quote=True,
-    )
