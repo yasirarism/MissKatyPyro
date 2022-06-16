b5c24b48db46c49305dcb825cd5e1327dd2ead6e
yasir <yasiramunandar@gmail.com>
2022-06-16 12:29:19 +0700

    fix

--- bot/plugins/mediainfo.py
+++ bot/plugins/mediainfo.py
@@ -1,5 +1,6 @@
 import io
 import json
+from os import remove as osremove
 import time
 import requests
 import aiohttp
@@ -15,22 +16,26 @@ from bot.utils.pyro_progress import (
     humanbytes,
 )
 
-@Client.on_message(filters.command(["mediainfo","mediainfo@MissKatyRoBot"], COMMAND_HANDLER))
+
+@Client.on_message(
+    filters.command(["mediainfo", "mediainfo@MissKatyRoBot"], COMMAND_HANDLER))
 @capture_err
 async def mediainfo(client, message):
-    if reply := message.reply_to_message:
-        process = await message.reply_text("`Sedang memproses, lama waktu tergantung ukuran file kamu...`")
+    if message.reply_to_message and message.reply_to_message.media:
+        process = await message.reply_text(
+            "`Sedang memproses, lama waktu tergantung ukuran file kamu...`")
         x_media = None
         file_info = get_file_id(message.reply_to_message)
         if file_info is None:
-           await process.edit_text("Balas ke format media yang valid")
-           return
+            await process.edit_text("Balas ke format media yang valid")
+            return
         c_time = time.time()
         # file_path = safe_filename(await reply.download())
         file_path = await client.download_media(
             message=message.reply_to_message,
             progress=progress_for_pyrogram,
-            progress_args=("trying to download, sabar yakk..", process, c_time),
+            progress_args=("trying to download, sabar yakk..", process,
+                           c_time),
         )
         output_ = await runcmd(f'mediainfo "{file_path}"')
         out = output_[0] if len(output_) != 0 else None
@@ -45,16 +50,24 @@ async def mediainfo(client, message):
         title = "MissKaty Bot Mediainfo"
         text_ = file_info.message_type
         link = post_to_telegraph(title, body_text)
-        markup = InlineKeyboardMarkup([[InlineKeyboardButton(text=text_, url=link)]])
+        markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text=text_, url=link)]])
         await process.edit_text("ℹ️ <b>MEDIA INFO</b>", reply_markup=markup)
+        try:
+            osremove(file_path)
+        except:
+            pass
     else:
         try:
             link = message.text.split(" ", maxsplit=1)[1]
             process = await message.reply_text("`Mohon tunggu sejenak...`")
             try:
-              output = subprocess.check_output(["mediainfo", f"{link}"]).decode('utf-8')
+                output = subprocess.check_output(["mediainfo",
+                                                  f"{link}"]).decode('utf-8')
             except Exception:
-              return await process.edit("Sepertinya link yang kamu kirim tidak valid, pastikan direct link dan bisa di download.")
+                return await process.edit(
+                    "Sepertinya link yang kamu kirim tidak valid, pastikan direct link dan bisa di download."
+                )
             title = "MissKaty Bot Mediainfo"
             body_text = f"""
                          <img src='https://telegra.ph/file/72c99bbc89bbe4e178cc9.jpg' />
@@ -65,10 +78,17 @@ async def mediainfo(client, message):
             # response = requests.post(siteurl, data={"content": output, "extension": 'txt'} )
             # response = response.json()
             # spacebin = "https://spaceb.in/"+response['payload']['id']
-            markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="💬 Telegraph", url=tgraph)]])
+            markup = InlineKeyboardMarkup(
+                [[InlineKeyboardButton(text="💬 Telegraph", url=tgraph)]])
             with io.BytesIO(str.encode(output)) as out_file:
-               out_file.name = "MissKaty_Mediainfo.txt"
-               await message.reply_document(out_file, caption=f"Hasil mediainfo anda..\n\n<b>Request by:</b> {message.from_user.mention}", reply_markup=markup)
-               await process.delete()
+                out_file.name = "MissKaty_Mediainfo.txt"
+                await message.reply_document(
+                    out_file,
+                    caption=
+                    f"Hasil mediainfo anda..\n\n<b>Request by:</b> {message.from_user.mention}",
+                    reply_markup=markup)
+                await process.delete()
         except IndexError:
-            return await message.reply_text("Gunakan command /mediainfo [link], atau reply telegram media dengan /mediainfo.")
+            return await message.reply_text(
+                "Gunakan command /mediainfo [link], atau reply telegram media dengan /mediainfo."
+            )
