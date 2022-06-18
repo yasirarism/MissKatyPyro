5225b46e2a55660d70b9b214b405c779e2bffa2f
yasir <yasiramunandar@gmail.com>
2022-06-18 14:37:20 +0700

    kang

--- bot/utils/stickerset.py
+++ bot/utils/stickerset.py
@@ -25,7 +25,7 @@ from pyrogram import Client, errors, raw
 async def get_sticker_set_by_name(client: Client,
                                   name: str) -> raw.base.messages.StickerSet:
     try:
-        return await client.send(
+        return await client.invoke(
             raw.functions.messages.GetStickerSet(
                 stickerset=raw.types.InputStickerSetShortName(short_name=name),
                 hash=0,
@@ -46,7 +46,7 @@ async def create_sticker_set(
     short_name: str,
     stickers: List[raw.base.InputStickerSetItem],
 ) -> raw.base.messages.StickerSet:
-    return await client.send(
+    return await client.invoke(
         raw.functions.stickers.CreateStickerSet(
             user_id=await client.resolve_peer(owner),
             title=title,
@@ -60,7 +60,7 @@ async def add_sticker_to_set(
     stickerset: raw.base.messages.StickerSet,
     sticker: raw.base.InputStickerSetItem,
 ) -> raw.base.messages.StickerSet:
-    return await client.send(
+    return await client.invoke(
         raw.functions.stickers.AddStickerToSet(
             stickerset=raw.types.InputStickerSetShortName(
                 short_name=stickerset.set.short_name),
