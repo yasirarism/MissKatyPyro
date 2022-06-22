3c6f129752b6c76a3590056c2862db29191a8c48
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-06-22 20:21:43 +0700

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -1,7 +1,9 @@
 import time
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from info import COMMAND_HANDLER
 from bot import app, user, botStartTime
+from bot.plugins.dev import shell_exec
 from bot.utils.human_read import get_readable_time
 from subprocess import check_output
 
@@ -14,7 +16,17 @@ async def ping(_, message):
     rm = await message.reply_text("🐱 Pong!!...")
     end_t = time.time()
     time_taken_s = round(end_t - start_t, 3)
+    keyb = InlineKeyboardMarkup([[InlineKeyboardButton("Stats", callback_data=f"ping_{message.from_user.id}")]]))
     try:
         await rm.edit(f"<b>🐈 MissKaty {botVersion} online.</b> (<b>Last Commit:</b> <code>{last_commit}</code>)\n\n<b>Ping:</b> <code>{time_taken_s} detik</code>\n<b>Uptime:</b> <code>{currentTime}</code>", disable_web_page_preview=True)
     except Exception:
         pass
+
+@app.on_callback_query(filters.regex('^ping'))
+async def callback_ping(_, query):
+    i, user = query.data.split('_')
+    if user == f"{query.from_user.id}":
+        neofetch = (await shell_exec("neofetch --stdout"))[0]
+        await query.answer(neofetch, show_alert=True)
+    else:
+        await query.answer("Hayoo mau mainan tombol yakk.. 😉", show_alert=True)
