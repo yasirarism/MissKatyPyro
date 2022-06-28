7d3a60df455169a77b2bbd61fafbc0e86990cb15
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-06-28 12:25:25 +0700

    Update ffmpeg_helper.py

--- bot/utils/ffmpeg_helper.py
+++ bot/utils/ffmpeg_helper.py
@@ -76,7 +76,6 @@ async def genss_link(
 ):
     metadata = (await shell_exec(f"ffprobe -i {video_link} -show_entries format=duration -v quiet -of csv='p=0'"))[0]
     duration = round(float(metadata))
-    logging.info(duration)
     if duration > min_duration:
         images = []
         ttl_step = duration // no_of_photos
