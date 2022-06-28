ba8e5d2a6e5cb9c8af078a966c668cdc37d0ba0e
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-06-28 11:03:44 +0700

    Update ffmpeg_helper.py

--- bot/utils/ffmpeg_helper.py
+++ bot/utils/ffmpeg_helper.py
@@ -5,6 +5,10 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from pyrogram.types import InputMediaPhoto
 
+def hhmmss(seconds):
+    x = time.strftime('%H:%M:%S',time.gmtime(seconds))
+    return x
+
 async def take_screen_shot(video_file, output_directory, ttl):
     # https://stackoverflow.com/a/13891070/4723940
     out_put_file_name = output_directory + \
@@ -54,7 +58,7 @@ async def generate_screen_shots(
         for looper in range(0, no_of_photos):
             ss_img = await take_screen_shot(video_file, output_directory, current_ttl)
             current_ttl = current_ttl + ttl_step
-            images.append(InputMediaPhoto(media=ss_img, caption=current_ttl))
+            images.append(InputMediaPhoto(media=ss_img, caption=f'Screenshot at {hhmmss(current_ttl)}'))
             await msg.edit(f"📸 <b>Take Screenshoot:</b>\n<code>{looper+1} of {no_of_photos} screenshot generated..</code>")
             await asyncio.sleep(1)
         return images
