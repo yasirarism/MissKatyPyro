be2686a28ae7a1d0b7197b07f70539d46f34025f
yasir <yasiramunandar@gmail.com>
2022-07-04 19:11:55 +0700

    up

--- bot/plugins/json.py
+++ bot/plugins/json.py
@@ -1,8 +1,8 @@
-import os
+import os, json
 from pyrogram import filters
 from bot import ptb, app
 from info import COMMAND_HANDLER
-from telegram import Update
+from telegram import Update, constants
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
@@ -13,7 +13,9 @@ async def jsonify(_, message):
 
     the_real_message = message.reply_to_message or message
     try:
-        await message.reply_text(f"<code>{the_real_message}</code>")
+        await message.reply_text(
+            f"<code>{json.dumps(the_real_message, indent=2, ensure_ascii=False)}</code>",
+            parse_mode=constants.ParseMode.HTML)
     except Exception as e:
         with open("json.text", "w+", encoding="utf8") as out_file:
             out_file.write(str(the_real_message))
