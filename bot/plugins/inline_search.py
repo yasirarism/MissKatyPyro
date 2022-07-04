c9be2152e79bf45a9176a592daf1791990188df2
yasir <yasiramunandar@gmail.com>
2022-07-04 13:02:56 +0700

    update

--- bot/plugins/inline_search.py
+++ bot/plugins/inline_search.py
@@ -1,59 +1,12 @@
 import requests
 import json
 from bot import app
-from bot.utils.inline_func import *
 from bot.plugins.misc import get_content
 from utils import get_poster
 from pyrogram import filters, enums
 from bs4 import BeautifulSoup
 from gpytranslate import Translator
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InlineQuery, InlineQueryResultArticle, InputTextMessageContent
-
-
-@app.on_inline_query()
-async def inline_query_handler(client, query):
-    try:
-        text = query.query.strip().lower()
-        answers = []
-        if text.split()[0] == "google":
-            if len(text.split()) < 2:
-                return await client.answer_inline_query(
-                    query.id,
-                    results=answers,
-                    switch_pm_text="Google Search | google [QUERY]",
-                    switch_pm_parameter="help",
-                )
-            tex = text.split(None, 1)[1].strip()
-            answerss = await google_search_func(answers, tex)
-            await client.answer_inline_query(
-                query.id,
-                results=answerss,
-            )
-        elif text.split()[0] == "yt":
-            if len(text.split()) < 2:
-                return await client.answer_inline_query(
-                    query.id,
-                    results=answers,
-                    switch_pm_text="YouTube Search | yt [QUERY]",
-                    switch_pm_parameter="help",
-                )
-            tex = text.split(None, 1)[1].strip()
-            answerss = await youtube_func(answers, tex)
-            await client.answer_inline_query(query.id, results=answerss)
-
-        elif text.split()[0] == "lirik":
-            if len(text.split()) < 2:
-                return await client.answer_inline_query(
-                    query.id,
-                    results=answers,
-                    switch_pm_text="Lyrics Search | lyrics [QUERY]",
-                    switch_pm_parameter="help",
-                )
-            tex = text.split(None, 1)[1].strip()
-            answerss = await lyrics_func(answers, tex)
-            await client.answer_inline_query(query.id, results=answerss)
-    except Exception as e:
-        pass
 
 
 @app.on_inline_query(
