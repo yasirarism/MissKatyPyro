8033f08bf6db074b707b70a4bbcea69e4968d10c
yasir <yasiramunandar@gmail.com>
2022-07-04 20:35:18 +0700

    up

--- bot/__main__.py
+++ bot/__main__.py
@@ -6,16 +6,16 @@ from pyrogram import idle, __version__
 
 
 # Run Bot
-def main():
-    app.start()
-    user.start()
-    me = app.get_me()
-    ubot = user.get_me()
+async def main():
+    await app.start()
+    await user.start()
+    me = await app.get_me()
+    ubot = await user.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
     try:
-        app.send_message(
+        await app.send_message(
             617426792,
             f"USERBOT AND BOT STARTED with Pyrogram v{__version__}..\nUserBot: {ubot.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
         )
@@ -25,7 +25,7 @@ def main():
         f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
     )
     ptb.run_polling()
-    idle()
+    await idle()
 
 
 if __name__ == '__main__':
