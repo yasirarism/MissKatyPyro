65d3bbc8009d2b06cc05c69207c46ab6dcfe335e
yasir <yasiramunandar@gmail.com>
2022-07-04 19:24:51 +0700

    up

--- bot/plugins/json.py
+++ bot/plugins/json.py
@@ -13,9 +13,7 @@ async def jsonify(_, message):
 
     the_real_message = message.reply_to_message or message
     try:
-        await message.reply_text(
-            f"<code>{json.dumps(the_real_message, indent=2, ensure_ascii=False)}</code>",
-            parse_mode=constants.ParseMode.HTML)
+        await message.reply_text(f"<code>{the_real_message}</code>")
     except Exception as e:
         with open("json.text", "w+", encoding="utf8") as out_file:
             out_file.write(str(the_real_message))
@@ -32,7 +30,9 @@ async def json_ptb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     the_real_message = update.message.reply_to_message or update.message
     try:
-        await update.message.reply_text(f"<code>{the_real_message}</code>")
+        await update.message.reply_text(
+            f"<code>{json.dumps(the_real_message, indent=2, ensure_ascii=False)}</code>",
+            parse_mode="constants.ParseMode.HTML")
     except Exception as e:
         with open("json.text", "w+", encoding="utf8") as out_file:
             out_file.write(str(the_real_message))
