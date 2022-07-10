25999201a99b742f446e9e683975f1a953b759fc
yasir <yasiramunandar@gmail.com>
2022-07-10 21:25:00 +0700

    tt

--- bot/plugins/channel_search.py
+++ bot/plugins/channel_search.py
@@ -183,7 +183,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
             current = temp.CURRENT
             temp.CANCEL = False
             media = await bot.get_messages(chat, lst_msg_id, temp.CURRENT)
-            for message in media:
+            async for message in media:
                 if temp.CANCEL:
                     await msg.edit(
                         f"Successfully Cancelled!!\n\nSaved <code>{total_files}</code> files to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>"
