3c08cd72234a4a898b2ccf744401c4665c2d4caf
yasir <yasiramunandar@gmail.com>
2022-07-10 20:54:21 +0700

    tt

--- bot/plugins/pm_filter.py
+++ bot/plugins/pm_filter.py
@@ -17,7 +17,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp
 from database.users_chats_db import db
-from database.filters_mdb import (
+from database.ia_filterdb import (
     del_all,
     find_filter,
     get_filters,
--- database/yasirdb.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-import os
-from info import DATABASE_URI
-
-database = pymongo.MongoClient(DATABASE_URI)['notes']['notes']
