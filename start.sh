fb72b89fc04feab5e5b67f338ea2e3a0b157c433
cerminbot <76247250+cerminbot@users.noreply.github.com>
2022-07-17 19:28:37 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-# if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://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@github.com/yasirarism/MissKatyPyro.git /YasirBot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /YasirBot
- fi
-cd /YasirBot
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
 python3 -m bot
