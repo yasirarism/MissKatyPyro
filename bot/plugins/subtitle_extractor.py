2ac9cc2eaf6a42d9933aed19d3e4915f9a1dc7af
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-07-25 20:57:02 +0700

    Create subtitle_extractor.py

---
+++ bot/plugins/subtitle_extractor.py
@@ -0,0 +1,49 @@
+import json
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from bot.plugins.dev import shell_exec
+from bot import app
+from pyrogram import filters
+from info import COMMAND_HANDLER
+from bot.utils.decorator import capture_err
+
+@capture_err
+@app.on_message(filters.command(["extractsub"], COMMAND_HANDLER))
+async def extractsub(_, msg):
+  link = msg.text.split()[1]
+  pesan = await message.reply("Processing...")
+  res = (await shell_exec("ffprobe -loglevel 0 -print_format json -show_format -show_streams https://link.yasir.eu.org/unduh/19848/Gonjiam.Haunted.Asylum.2018.720p.BR+%40RickyChristanto.mkv"))[0]
+  details = json.loads(res)
+  buttons = []
+  DATA[f"{msg.chat.id}-{pesan.id}"] = {}
+  for stream in details["streams"]:
+    mapping = stream['index']
+    stream_name = stream['codec_name']
+    stream_type = stream['codec_type']
+    if stream_type in ("audio", "subtitle"):
+      pass
+    else:
+      continue
+    try:
+      lang = stream["tags"]["language"]
+    except:
+       lang = mapping
+    DATA[f"{msg.chat.id}-{pesan.id}"][int(mapping)] = {
+            "map" : mapping,
+            "name" : stream_name,
+            "type" : stream_type,
+            "lang" : lang,
+            "link" : link
+    }
+    buttons.append([
+            InlineKeyboardButton(
+                f"{stream_type.upper()} - {str(lang).upper()}", f"{stream_type}_{mapping}_{message.chat.id}-{msg.message_id}"
+            )
+    ])
+
+  buttons.append([
+        InlineKeyboardButton("CANCEL",f"cancel_{mapping}_{message.chat.id}-{msg.message_id}")
+  ])
+  await pesan.edit_text(
+        "**Select the Stream to be Extracted...**",
+        reply_markup=InlineKeyboardMarkup(buttons)
+  )
