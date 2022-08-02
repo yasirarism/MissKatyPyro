0f01b62547cb7e6c882825fb6b521b7cfb0974bd
yasir <yasiramunandar@gmail.com>
2022-08-02 21:20:14 +0700

    karma

--- bot/utils/admin_helper.py
+++ bot/utils/admin_helper.py
@@ -1,9 +1,14 @@
+import imp
 from pyrogram import enums
+from bot import app
+
 
 async def is_admin(group_id: int, user_id: int):
     try:
         user_data = await app.get_chat_member(group_id, user_id)
-        return user_data.status in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER]
+        return user_data.status in [
+            enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER
+        ]
     except:
         # print('Not admin')
         return False
