07bf72340554411c20bdb55ea82d8adcb8b22655
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-08-07 21:05:39 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,38 +1,33 @@
-import logging, asyncio
+import logging
+import threading
+from flask import Flask
 from bot import app, user
 from utils import temp
 from pyrogram.raw.all import layer
 from pyrogram import idle, __version__
 
-main_loop = asyncio.get_event_loop()
+web = Flask(__name__)
 
+@web.route("/")
+def me():
+    return str(app.get_me())
 
 # Run Bot
-async def main():
-    await app.start()
-    await user.start()
-    me = await app.get_me()
-    ubot = await user.get_me()
-    temp.ME = me.id
-    temp.U_NAME = me.username
-    temp.B_NAME = me.first_name
-    try:
-        await app.send_message(
-            617426792,
-            f"USERBOT AND BOT STARTED with Pyrogram v{__version__}..\nUserBot: {ubot.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
-        )
-    except:
-        pass
-    logging.info(
-        f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
+app.start()
+user.start()
+me = app.get_me()
+ubot = user.get_me()
+temp.ME = me.id
+temp.U_NAME = me.username
+temp.B_NAME = me.first_name
+try:
+    app.send_message(
+        617426792, f"USERBOT AND BOT STARTED with Pyrogram v{__version__}..\nUserBot: {ubot.first_name}\nBot: {me.first_name}\n\nwith Pyrogram v{__version__} (Layer {layer}) started on @{me.username}."
     )
-    await idle()
-    # ptb.run_polling()
-
-
-if __name__ == '__main__':
-    try:
-        main_loop.run_until_complete(main())
-    except KeyboardInterrupt:
-        logging.info(
-            '----------------------- Service Stopped -----------------------')
+except:
+  pass
+logging.info(f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
+threading.Thread(target=app.run, daemon=True).start()
+idle()
+app.stop()
+ubot.stop()
