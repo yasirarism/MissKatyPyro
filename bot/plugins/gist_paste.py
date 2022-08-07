c5f3dbaa4126e019950877733670b020d11f1f58
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-08-07 20:34:29 +0700

    Update gist_paste.py

--- bot/plugins/gist_paste.py
+++ bot/plugins/gist_paste.py
@@ -9,64 +9,6 @@ from bot import app
 from info import COMMAND_HANDLER
 
 
-class Github_Gist:  # Learn class for first time, PR if bad or want to improve
-
-    def __init__(
-            self,
-            title: str = "",
-            description: str = "",
-            is_secret: bool = False):  # All Params For Custom, Maybe Later
-        self.api = "https://api.github.com/gists"
-        self.token = "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"  # Github Token with Gist Create Permission
-        self.title = title
-        self.description = description
-        self.secret = is_secret
-        self.username = "MissKatyRoBot"
-        if not self.title:
-            self.title = f"Gist Paste by @{self.username}"
-        if not self.description:
-            self.description = f"Github Gist created by @{self.username} from Telegram"
-
-    def create(self, text: str) -> dict:
-        data = {
-            "description": self.description,
-            "public": self.secret,
-            "files": {
-                self.title: {
-                    "content": text
-                }
-            }
-        }
-        headers = {
-            "Authorization": f"token {self.token}",
-            "Content-Type": "application/json"
-        }
-        resp = post(self.api, headers=headers, data=dumps(data))
-        if any(x == resp.status_code for x in [200, 201]):
-            resp = resp.json()
-            if not resp.get('message'):
-                result = {
-                    "url":
-                    resp.get('html_url'),
-                    "raw":
-                    list(resp.get('files').values())[0].get('raw_url').replace(
-                        ' ', '%20')
-                }
-                return result
-            else:
-                raise Exception(f"ERROR : {resp.get('message')}")
-        else:
-            raise Exception(f"ERROR : Failed To Create Github Gist")
-
-    def delete(self, ids: str) -> str:
-        headers = {"Authorization": f"token {self.token}"}
-        resp = delete(self.api + '/' + ids, headers=headers)
-        if resp.ok:
-            return "Success Delete Gist With ID {}".format(ids)
-        else:
-            raise Exception(f"ERROR : Failed To Delete Github Gist")
-
-
 # Size Checker for Limit
 def humanbytes(size: int):
     """Convert Bytes To Bytes So That Human Can Read It"""
@@ -114,7 +56,7 @@ async def create(_, message):
         return await message.reply_text(
             f"**Reply To A Message With /{target} or with command**")
 
-    msg = await message.reply_text("`Pasting to Github Gist...`")
+    msg = await message.reply_text("`Pasting to Yasir Paste...`")
     data = ''
     limit = 1024 * 1024
     if reply and reply.document:
@@ -149,9 +91,12 @@ async def create(_, message):
         uname = message.sender_chat.title
 
     try:
-        resp = Github_Gist().create(data)
-        url = resp.get("url")
-        raw = resp.get("raw")
+        files = {
+            'c': (None, data),
+            'e': (None, '5d'),
+        }
+        resp = post('https://paste.yasir.eu.org', files=files)
+        url = resp.text
     except Exception as e:
         await msg.edit(f"`{e}`")
         return
@@ -159,34 +104,11 @@ async def create(_, message):
     if not url:
         return await msg.edit("Text Too Short Or File Problems")
     button = []
-    if raw is not None:
-        button.append([
-            InlineKeyboardButton("Open Link", url=url),
-            InlineKeyboardButton("Raw Link", url=raw)
-        ])
-    else:
-        button.append([InlineKeyboardButton("Open Link", url=url)])
+    button.append([InlineKeyboardButton("Open Link", url=url)])
     button.append([
         InlineKeyboardButton("Share Link",
                              url=f"https://telegram.me/share/url?url={url}")
     ])
 
-    pasted = f"**Your Github Gist URL successfully pasted<a href='{url}'>.</a>\n\nPaste by {uname}**"
+    pasted = f"**Successfully pasted to <a href='{url}'>.</a>\n\nPaste by {uname}**\nPaste will expired in 5 days."
     await msg.edit(pasted, reply_markup=InlineKeyboardMarkup(button))
-
-
-@app.on_message(
-    filters.command(["delgist"], COMMAND_HANDLER) & filters.user(617426792))
-async def delete(_, message):
-    try:
-        ids = message.command[1]
-    except IndexError:
-        return await message.reply_text(f"**/{message.command[0]} GIST_ID**",
-                                        quote=True)
-
-    try:
-        result = Github_Gist.delete(ids)
-    except Exception as e:
-        result = str(e)
-
-    return await message.reply_text(result)
