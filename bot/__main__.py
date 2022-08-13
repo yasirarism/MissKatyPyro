58a1b2ab063e86aeeb014206392844fa4b7803f6
yasir <yasiramunandar@gmail.com>
2022-08-13 22:28:09 +0700

    tes

--- bot/__main__.py
+++ bot/__main__.py
@@ -13,7 +13,7 @@ loop = asyncio.get_event_loop()
 async def start_services():
     await app.start()
     await user.start()
-    a = await asyncio.create_subprocess_exec('gunicorn web.wserver:web',
+    a = await asyncio.create_subprocess_exec(f'gunicorn web.wserver:web',
                                              stdout=asyncio.subprocess.PIPE,
                                              stderr=asyncio.subprocess.PIPE)
     stdout, stderr = await a.communicate()
