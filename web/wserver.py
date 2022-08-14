a5aa90e56ad1ef42d8702633663e280474ca8433
yasir <yasiramunandar@gmail.com>
2022-08-14 19:15:29 +0700

    fix

--- web/wserver.py
+++ web/wserver.py
@@ -1,18 +1,15 @@
-from flask import Flask, request
-from bot import app, user
+# from flask import Flask, request
+# from bot import app, user
 
-web = Flask(__name__)
+# web = Flask(__name__)
 
+# @web.route('/')
+# async def homepage():
+#     return "Hai guys.."
 
-@web.route('/')
-async def homepage():
-    return "Hai guys.."
+# @web.route('/me')
+# async def me():
+#     return str(await user.get_me())
 
-
-@web.route('/me')
-async def me():
-    return str(await user.get_me())
-
-
-if __name__ == "__main__":
-    web.run()
+# if __name__ == "__main__":
+#     web.run()
