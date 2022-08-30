8f539c4bd77779410b54be533d404ae488aacba4
cerminbot <76247250+cerminbot@users.noreply.github.com>
2022-08-30 19:41:50 +0700

    Update tools.py

--- bot/utils/tools.py
+++ bot/utils/tools.py
@@ -6,3 +6,18 @@ def get_random_string(length):
     letters = string.ascii_lowercase
     result_str = "".join(random.choice(letters) for i in range(length))
     return result_str
+
+def rentry(teks):
+    # buat dapetin cookie
+    session = requests.Session()
+    response = session.get('https://rentry.co')
+    kuki = session.cookies.get_dict()
+    # headernya
+    header = {"Referer": 'https://rentry.co'}
+
+    payload = {'csrfmiddlewaretoken': kuki['csrftoken'], 'text': teks}
+    res = requests.post('https://rentry.co/api/new',
+                        payload,
+                        headers=header,
+                        cookies=kuki).json().get("url")
+    return res
