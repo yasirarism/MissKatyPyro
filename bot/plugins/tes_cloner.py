9c4dde77c83736204a13c9840a37b49ff0c35889
yasir <yasiramunandar@gmail.com>
2022-08-30 11:50:57 +0700

    ff

--- bot/plugins/tes_cloner.py
+++ bot/plugins/tes_cloner.py
@@ -1,6 +1,7 @@
 from bot import app
 from info import API_HASH, API_ID
 from pyrogram import filters, idle, Client
+from bot.utils.tools import get_random_string
 
 
 @app.on_message(filters.command("clonebot"))
@@ -9,17 +10,18 @@ async def clone(client, message):
         return await message.reply("Usage:\n\n /clonebot token")
     token = message.command[1]
     text = await message.reply("Cloning bot..")
+    client_name = get_random_string(4)
     try:
         await text.edit("Booting Your Client")
         # change this Directry according to ur repo
-        client = Client(":memory:",
-                        API_ID,
-                        API_HASH,
-                        bot_token=token,
-                        plugins={"root": "bot/clone_plugins"})
-        await client.start()
+        client_name = Client(":memory:",
+                             API_ID,
+                             API_HASH,
+                             bot_token=token,
+                             plugins={"root": "bot/clone_plugins"})
+        await client_name.start()
         idle()
-        user = await client.get_me()
+        user = await client_name.get_me()
         await text.edit(
             f"Your Client Has Been Successfully Started As @{user.username}! ✅\n\nThanks for Cloning."
         )
