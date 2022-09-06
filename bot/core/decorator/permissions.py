7a97cc54ec5ededa2a5b2768af6216288c0b9e85
cerminbot <76247250+cerminbot@users.noreply.github.com>
2022-09-06 12:04:44 +0700

    Update permissions.py

--- bot/core/decorator/permissions.py
+++ bot/core/decorator/permissions.py
@@ -10,27 +10,28 @@ from time import time
 async def member_permissions(chat_id: int, user_id: int):
     perms = []
     try:
-        member = (await app.get_chat_member(chat_id, user_id)).privileges
+        member = await app.get_chat_member(chat_id, user_id)
+        perijinan = member.privileges
     except Exception:
         return []
     if member.status != enums.ChatMemberStatus.MEMBER:
-        if member.can_post_messages:
+        if perijinan.can_post_messages:
             perms.append("can_post_messages")
-        if member.can_edit_messages:
+        if perijinan.can_edit_messages:
             perms.append("can_edit_messages")
-        if member.can_delete_messages:
+        if perijinan.can_delete_messages:
             perms.append("can_delete_messages")
-        if member.can_restrict_members:
+        if perijinan.can_restrict_members:
             perms.append("can_restrict_members")
-        if member.can_promote_members:
+        if perijinan.can_promote_members:
             perms.append("can_promote_members")
-        if member.can_change_info:
+        if perijinan.can_change_info:
             perms.append("can_change_info")
-        if member.can_invite_users:
+        if perijinan.can_invite_users:
             perms.append("can_invite_users")
-        if member.can_pin_messages:
+        if perijinan.can_pin_messages:
             perms.append("can_pin_messages")
-        if member.can_manage_video_chats:
+        if perijinan.can_manage_video_chats:
             perms.append("can_manage_video_chats")
     return perms
 
