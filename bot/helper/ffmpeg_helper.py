7b4dbd348aa6dd4eeb867d520c806a6114456fcc
cerminbot <76247250+cerminbot@users.noreply.github.com>
2022-09-07 21:40:34 +0700

    tes

--- bot/helper/ffmpeg_helper.py
+++ bot/helper/ffmpeg_helper.py
@@ -13,10 +13,9 @@ def hhmmss(seconds):
     return x
 
 
-async def take_ss(video_file, output_directory, ttl):
-    # https://stackoverflow.com/a/13891070/4723940
-    out_put_file_name = output_directory + "/" + str(time.time()) + ".jpg"
-    file_genertor_command = ["ffmpeg", "-ss", str(ttl), "-i", video_file, "-vframes", "1", out_put_file_name]
+async def take_ss(video_file):
+    out_put_file_name = f"genss{str(time.time())}.png"
+    file_genertor_command = ["vcsi", video_file, "-t", "-w", "850", "-g", "3x5", "--end-delay-percent", "20", "-o", out_put_file_name]
     process = await asyncio.create_subprocess_exec(
         *file_genertor_command,
         stdout=asyncio.subprocess.PIPE,
@@ -32,7 +31,7 @@ async def take_ss(video_file, output_directory, ttl):
 
 
 async def ssgen_link(video, output_directory, ttl):
-    out_put_file_name = output_directory + "/" + str(time.time()) + ".jpg"
+    out_put_file_name = output_directory + "/" + str(time.time()) + ".png"
     cmd = [
         "ffmpeg",
         "-ss",
@@ -65,31 +64,6 @@ async def genss_link(msg, video_link, output_directory, min_duration, no_of_phot
         current_ttl = ttl_step
         for looper in range(0, no_of_photos):
             ss_img = await ssgen_link(video_link, output_directory, current_ttl)
-            images.append(InputMediaPhoto(media=ss_img, caption=f"Screenshot at {hhmmss(current_ttl)}"))
-            try:
-                await msg.edit(f"📸 <b>Take Screenshoot:</b>\n<code>{looper+1} of {no_of_photos} screenshot generated..</code>")
-            except FloodWait as e:
-                await asyncio.sleep(e.value)
-                await msg.edit(f"📸 <b>Take Screenshoot:</b>\n<code>{looper+1} of {no_of_photos} screenshot generated..</code>")
-            current_ttl = current_ttl + ttl_step
-            await asyncio.sleep(2)
-        return images
-    else:
-        return None
-
-
-async def generate_screen_shots(msg, video_file, output_directory, min_duration, no_of_photos):
-    metadata = extractMetadata(createParser(video_file))
-    duration = 0
-    if metadata is not None:
-        if metadata.has("duration"):
-            duration = metadata.get("duration").seconds
-    if duration > min_duration:
-        images = []
-        ttl_step = duration // no_of_photos
-        current_ttl = ttl_step
-        for looper in range(0, no_of_photos):
-            ss_img = await take_ss(video_file, output_directory, current_ttl)
             images.append(InputMediaPhoto(media=ss_img, caption=f"Screenshot at {hhmmss(current_ttl)}"))
             try:
                 await msg.edit(f"📸 <b>Take Screenshoot:</b>\n<code>{looper+1} of {no_of_photos} screenshot generated..</code>")
