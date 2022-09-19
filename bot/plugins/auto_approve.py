0aad5cab925ea4c3aa6f7a55634d847a2fcdd2c0
cerminbot <76247250+cerminbot@users.noreply.github.com>
2022-09-19 11:20:36 +0700

    ff

--- bot/plugins/auto_approve.py
+++ bot/plugins/auto_approve.py
@@ -1,17 +1,17 @@
 from bot import app
 from pyrogram import filters
 from pyrogram.types import ChatJoinRequest, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from pyrogram.errors import 
+from pyrogram.errors import UserIsBlocked
 from bot.core.decorator.errors import capture_err
 
 @capture_err
 @app.on_chat_join_request(filters.chat(-1001686184174))
 async def approve_join_chat(c, m):
     # try:
-        markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Sudah/Already", callback_data=f"approve_{m.chat.id}"), InlineKeyboardButton(text="Belum/Not Yet", callback_data=f"declined_{m.chat.id}")]])
+        markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Sudah", callback_data=f"approve_{m.chat.id}"), InlineKeyboardButton(text="Belum/Not Yet", callback_data=f"declined_{m.chat.id}")]])
         await c.send_message(
             m.from_user.id,
-            "<b>PERMINTAAN JOIN CHANNEL YMOVIEZ REBORN</b>\n\nID: Sebelum masuk ke channel, apakah anda sudah membaca catatan di https://t.me/YMovieZ_New/4 ? Jika sudah silahkan klik <b>Sudah</b> ..\n\nEN: Have you read the notes in https://t.me/YMovieZ_New/4 ? If so, please click <b>Already</b>",
+            "<b>PERMINTAAN JOIN CHANNEL YMOVIEZ REBORN</b>\n\nSebelum masuk ke channel ada tes kejujuran, apakah anda sudah membaca catatan di @YMovieZ_New? Jika sudah silahkan klik <b>Sudah</b>, jika kamu berbohong resiko kamu tanggung sendiri 😶‍🌫️.\n\nBot by @YasirPediaChannel",
             disable_web_page_preview=True,
             reply_markup=markup,
         )
@@ -21,14 +21,14 @@ async def approve_join_chat(c, m):
 @app.on_callback_query(filters.regex(r"^approve"))
 async def approve_chat(c, q):
     i, chat = q.data.split("_")
-    await q.message.edit("Yeayy, selamat kamu bisa bergabung di Channel YMovieZ Reborn. Jangan lupa share yakk biar makin banyak subnya..")
+    await q.message.edit("Yeayy, selamat kamu bisa bergabung di Channel YMovieZ Reborn...")
     await c.approve_chat_join_request(chat, q.from_user.id)
 
 
 @app.on_callback_query(filters.regex(r"^declined"))
 async def decline_chat(c, q):
     i, chat = q.data.split("_")
-    await q.message.edit("Yahh, sayang banget kamu ga jadi subs ke channel ini karena menekan tombol tolak..")
+    await q.message.edit("Yahh, kamu ditolak join channel. Biasakan rajin membaca yahhh..")
     await c.decline_chat_join_request(chat, q.from_user.id)
 
 
