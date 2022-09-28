f7b5a2b5784520acc436331a96f72dd14fde6146
cerminbot <76247250+cerminbot@users.noreply.github.com>
2022-09-28 10:46:20 +0700

    fx

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM
-from imdb import IMDb
 import asyncio
 from pyrogram.types import Message
 from typing import Union
@@ -19,7 +18,6 @@ logger.setLevel(logging.INFO)
 
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))")
 
-imdb = IMDb()
 
 BANNED = {}
 SMART_OPEN = "“"
@@ -50,86 +48,6 @@ async def is_subscribed(bot, query):
             return True
 
     return False
-
-
-async def get_poster(query, bulk=False, id=False, file=None):
-    if not id:
-        # https://t.me/GetTGLink/4183
-        query = (query.strip()).lower()
-        title = query
-        year = re.findall(r"[1-2]\d{3}$", query, re.IGNORECASE)
-        if year:
-            year = list_to_str(year[:1])
-            title = (query.replace(year, "")).strip()
-        elif file is not None:
-            year = re.findall(r"[1-2]\d{3}", file, re.IGNORECASE)
-            if year:
-                year = list_to_str(year[:1])
-        else:
-            year = None
-        movieid = imdb.search_movie(title.lower(), results=10)
-        if not movieid:
-            return None
-        if year:
-            filtered = list(filter(lambda k: str(k.get("year")) == str(year), movieid))
-            if not filtered:
-                filtered = movieid
-        else:
-            filtered = movieid
-        movieid = list(filter(lambda k: k.get("kind") in ["movie", "tv series"], filtered))
-        if not movieid:
-            movieid = filtered
-        if bulk:
-            return movieid
-        movieid = movieid[0].movieID
-    else:
-        movieid = int(query)
-    movie = imdb.get_movie(movieid)
-    if movie.get("original air date"):
-        date = movie["original air date"]
-    elif movie.get("year"):
-        date = movie.get("year")
-    else:
-        date = "N/A"
-    plot = ""
-    if not LONG_IMDB_DESCRIPTION:
-        plot = movie.get("plot")
-        if plot and len(plot) > 0:
-            plot = plot[0]
-    else:
-        plot = movie.get("plot outline")
-    if plot and len(plot) > 450:
-        plot = plot[:450] + "..."
-
-    return {
-        "title": movie.get("title"),
-        "votes": movie.get("votes"),
-        "aka": list_to_str(movie.get("akas")),
-        "seasons": movie.get("number of seasons"),
-        "box_office": movie.get("box office"),
-        "localized_title": movie.get("localized title"),
-        "kind": movie.get("kind"),
-        "imdb_id": f"tt{movie.get('imdbID')}",
-        "cast": list_to_str(movie.get("cast")),
-        "runtime": list_to_str(movie.get("runtimes")),
-        "countries": list_to_str(movie.get("countries")),
-        "certificates": list_to_str(movie.get("certificates")),
-        "languages": list_to_str(movie.get("languages")),
-        "director": list_to_str(movie.get("director")),
-        "writer": list_to_str(movie.get("writer")),
-        "producer": list_to_str(movie.get("producer")),
-        "composer": list_to_str(movie.get("composer")),
-        "cinematographer": list_to_str(movie.get("cinematographer")),
-        "music_team": list_to_str(movie.get("music department")),
-        "distributors": list_to_str(movie.get("distributors")),
-        "release_date": date,
-        "year": movie.get("year"),
-        "genres": list_to_str(movie.get("genres")),
-        "poster": movie.get("full-size cover url"),
-        "plot": plot,
-        "rating": str(movie.get("rating")),
-        "url": f"https://www.imdb.com/title/tt{movieid}",
-    }
 
 async def broadcast_messages(user_id, message):
     try:
