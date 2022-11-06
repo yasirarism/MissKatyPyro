2bba60d5b9884244b8fbeb8a368b9919f1028b94
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-11-06 20:28:03 +0700

    sss

--- bot/helper/ytdl_helper.py
+++ bot/helper/ytdl_helper.py
@@ -1,5 +1,59 @@
 import string
+import os
+import requests
+import time
 import random
 
 def random_char(y):
     return ''.join(random.choice(string.ascii_letters) for _ in range(y))
+
+import os
+import requests
+import time
+from Uploader.helper_funcs.display_progress import (
+    humanbytes
+)
+
+
+def DetectFileSize(url):
+    r = requests.get(url, allow_redirects=True, stream=True)
+    return int(r.headers.get("content-length", 0))
+
+
+def DownLoadFile(
+    url,
+    file_name,
+    chunk_size,
+    client,
+    ud_type,
+    message_id,
+    chat_id
+):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    if not url:
+        return file_name
+    r = requests.get(url, allow_redirects=True, stream=True)
+    # https://stackoverflow.com/a/47342052/4723940
+    total_size = int(r.headers.get("content-length", 0))
+    downloaded_size = 0
+    with open(file_name, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            if chunk:
+                fd.write(chunk)
+                downloaded_size += chunk_size
+            if (
+                client is not None
+                and ((total_size // downloaded_size) % 5) == 0
+            ):
+                time.sleep(0.3)
+                try:
+                    client.edit_message_text(
+                        chat_id,
+                        message_id,
+                        text=f"{ud_type}: {humanbytes(downloaded_size)} of {humanbytes(total_size)}",
+                    )
+
+                except:
+                    pass
+    return file_name
