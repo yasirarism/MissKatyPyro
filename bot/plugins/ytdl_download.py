db1865a89f83e090f3076a1758e800f2632ea5b5
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-11-06 20:40:06 +0700

    ss

--- bot/plugins/ytdl_download.py
+++ bot/plugins/ytdl_download.py
@@ -48,7 +48,7 @@ async def ytdown(_, message):
             x_reponse, _ = x_reponse.split("\n")
         response_json = json.loads(x_reponse)
         randem = random_char(5)
-        if os.path.exists("./YT_Down"):
+        if os.path.exists("/YT_Down"):
             os.mkdir("./YT_Down")
         save_ytdl_json_path = f"./YT_Down/{str(message.from_user.id)}{randem}.json"
         with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
