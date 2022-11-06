27327f0c95c0fad59caa03e7233fcf604137c2d1
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-11-06 22:29:22 +0700

    forget

--- bot/plugins/ytdl_download.py
+++ bot/plugins/ytdl_download.py
@@ -245,7 +245,7 @@ async def youtube_dl_call_back(bot, update):
             shutil.rmtree(tmp_directory_for_each_user, ignore_errors=True)
             asyncio.create_task(clendir(thumb_image_path))
             asyncio.create_task(clendir(tmp_directory_for_each_user))
-            asyncio.sleep(5)
+            await asyncio.sleep(5)
             await update.message.delete()
 
 
