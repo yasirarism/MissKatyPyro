ade1f877df255a7db164789f04c694395029e743
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-12-01 08:52:43 +0700

    Update vars.py

--- misskaty/vars.py
+++ misskaty/vars.py
@@ -12,8 +12,6 @@ def getConfig(name: str):
     except:
         return ""
 
-SESSION_PYRO = "AgD4NZsAT5Grbqm0QF1byf6IyKTrOe-225NXLb8QoewMiFzJkX-RieZjojz6XeVL1psCBEhPCt5xwTqeSg-PlTwesMlCfpiaCZ5EBVnDd_zXFSvClOVQUDYTZ4WYE4ZwiszMDShv74PG5G1lxCzYVhZZwFg4QELsJY6P4es8ctRknfdgbov-rck5UhbYrGkgKyM3LaZOgUczBIZuPeQp2Z56l1t78yjUBEaUi1G3zFWQUdxjffq5FbmB26clLo7QhxUErmAU5QXnvhuIWmZYKL_ts-N8uyX0GYUBCiPbAlgSmdpclWENJRNHGKyYMffLkfV2_bwvBMOVs3DbYqggukeVJEXsLgAAAAB4ss-MAA"
-
 # Required ENV
 try:
     BOT_TOKEN = getConfig("BOT_TOKEN")
@@ -23,7 +21,7 @@ try:
     DATABASE_URI = getConfig("DATABASE_URI")
     DATABASE_NAME = getConfig("DATABASE_NAME")
     LOG_CHANNEL = int(getConfig("LOG_CHANNEL"))
-    USER_SESSION = environ.get("USER_SESSION", SESSION_PYRO)
+    USER_SESSION = getConfig("USER_SESSION")
 except Exception as e:
     log_error(f"One or more env variables missing! Exiting now.\n{e}")
     exit(1)
