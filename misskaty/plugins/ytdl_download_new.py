a60011b1de84122397a756274d57f32052aaacc4
yasir <yasiramunandar@gmail.com>
2022-12-10 21:06:11 +0700

    FIx

--- misskaty/plugins/ytdl_download_new.py
+++ misskaty/plugins/ytdl_download_new.py
@@ -123,8 +123,9 @@ async def ytdl_gendl_callback(_, cq: CallbackQuery):
     callback = cq.data.split("|")
     key = callback[1]
     if callback[0] == "yt_gen":
-        if match := regex.match(cq.message.reply_to_message.command[1]) or regex.match(
-            key
+        if (
+            match := regex.match(cq.message.reply_to_message.command[1])
+            or len(callback) == 2
         ):
             x = await main.Extractor().get_download_button(key)
             await cq.edit_message_caption(caption=x.caption, reply_markup=x.buttons)
