a94590b908fe2de7765aa88f75b6ce66556fced3
yasir <yasiramunandar@gmail.com>
2023-01-28 22:20:26 +0700

    Fix

--- database/sangmata_db.py
+++ database/sangmata_db.py
@@ -8,7 +8,7 @@ async def cek_userdata(user_id: int) -> bool:
 
 async def get_userdata(user_id: int) -> bool:
     user = await matadb.find_one({"user_id": user_id})
-    return user["username"]
+    return user["username"], user["first_name"], user["last_name"]
 
-async def add_userdata(user_id: int, username):
-    await matadb.update_one({"user_id": user_id}, {"$set": {"username": username}}, upsert=True)
+async def add_userdata(user_id: int, username, first_name, last_name):
+    await matadb.update_one({"user_id": user_id}, {"$set": {"username": username, "first_name": first_name, "last_name": last_name}}, upsert=True)
--- misskaty/plugins/sangmata.py
+++ misskaty/plugins/sangmata.py
@@ -8,10 +8,19 @@ from database.sangmata_db import *
     group=3,
 )
 async def cek_mataa(_, m):
-    await m.reply("aaaa")
     if not await cek_userdata(m.from_user.id):
-        return await add_userdata(m.from_user.id, m.from_user.username)
-    username = await get_userdata(m.from_user.id)
+        return await add_userdata(m.from_user.id, m.from_user.username, m.from_user.first_name, m.from_user.last_name)
+    username, first_name, last_name = await get_userdata(m.from_user.id)
+    msg == ""
+    if username != m.from_user.username or first_name != m.from_user.first_name or last_name != m.from_user.last_name:
+        msg += "👀 <b>Mata MissKaty</b>\n\n"
     if username != m.from_user.username:
-        await m.reply(f"{m.from_user.mention} mengganti username dari {username} menjadi {m.from_user.username}")
-        await add_userdata(m.from_user.id, m.from_user.username)
+        msg += f"{m.from_user.mention} mengganti username dari {username} menjadi {m.from_user.username}.\n"
+        await add_userdata(m.from_user.id, m.from_user.username, m.from_user.first_name, m.from_user.last_name)
+    if first_name != m.from_user.first_name:
+        msg += f"{m.from_user.mention} mengganti username dari {username} menjadi {m.from_user.username}.\n"
+        await add_userdata(m.from_user.id, m.from_user.username, m.from_user.first_name, m.from_user.last_name)
+    if last_name != m.from_user.last_name:
+        msg += f"{m.from_user.mention} mengganti username dari {username} menjadi {m.from_user.username}."
+        await add_userdata(m.from_user.id, m.from_user.username, m.from_user.first_name, m.from_user.last_name)
+    await m.reply(msg)
