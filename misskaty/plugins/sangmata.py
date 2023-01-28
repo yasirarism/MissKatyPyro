2e3d5a1bb75f7a91d996b90293122d47f6d8d539
yasir <yasiramunandar@gmail.com>
2023-01-28 21:59:31 +0700

    Typo

--- misskaty/plugins/sangmata.py
+++ misskaty/plugins/sangmata.py
@@ -9,7 +9,8 @@ from database.sangmata_db import *
 )
 async def cek_mataa(_, m):
     if not cek_userdata(m.from_user.id):
-        return await add_userdata(m.from_user.id, m.from_user.username)
+        await add_userdata(m.from_user.id, m.from_user.username)
+        return
     username = await get_userdata(m.from_user.id)
     if username != m.from_user.username:
         await m.reply(f"{m.from_user.mention} mengganti username dari {username} menjadi {m.from_user.username}")
