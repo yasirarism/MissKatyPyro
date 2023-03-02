f62bd5fa536591092ed270bbaf6f866783860ad9
yasir <yasiramunandar@gmail.com>
2023-03-02 10:25:36 +0700

    Fix updater

--- update.py
+++ update.py
@@ -1,7 +1,26 @@
 from subprocess import run as srun 
-import logging, os
+import logging, os, requests
+from dotenv import load_dotenv
 
 LOGGER = logging.getLogger(__name__)
+
+CONFIG_FILE_URL = os.environ.get("CONFIG_FILE_URL", "")
+try:
+    if len(CONFIG_FILE_URL) == 0:
+        raise TypeError
+    try:
+        res = requests.get(CONFIG_FILE_URL)
+        if res.status_code == 200:
+            with open("config.env", "wb+") as f:
+                f.write(res.content)
+        else:
+            LOGGER.error(f"config.env err: {res.status_code}")
+    except Exception as e:
+        LOGGER.error(f"ENV_URL: {e}")
+except:
+    pass
+
+load_dotenv("config.env", override=True)
 
 UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL", "")
 if len(UPSTREAM_REPO_URL) == 0:
