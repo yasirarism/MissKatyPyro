1ded121876f94a535fd7628c876f2252db10dc65
yasir <yasiramunandar@gmail.com>
2023-03-12 19:40:26 +0700

    Some cmd

--- misskaty/plugins/dev.py
+++ misskaty/plugins/dev.py
@@ -143,7 +143,7 @@ async def shell(_, m):
 @app.on_edited_message((filters.command(["ev", "run"]) | filters.regex(r"app.run\(\)$")) & filters.user(SUDO))
 @user.on_message(filters.command(["ev", "run"], ".") & filters.me)
 async def evaluation_cmd_t(_, m):
-    if m.command and len(m.command) == 1:
+    if (m.command and len(m.command) == 1) or m.text == "app.run()":
         return await edit_or_reply(m, text="__No evaluate message!__")
     cmd = m.text.split(" ", 1)[1] if m.command else m.text.split("app.run()")[0]
     status_message = await editPesan(m, "<i>Processing eval pyrogram..</i>") if m.from_user.is_self else await kirimPesan(m, "<i>Processing eval pyrogram..</i>")
