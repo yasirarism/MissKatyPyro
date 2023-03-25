9da3f655c8b91cf11cf0399f5d7e6acceac491ed
yasir <yasiramunandar@gmail.com>
2023-03-25 12:00:40 +0700

    try add multi lang

--- database/locale_db.py
+++ database/locale_db.py
@@ -7,7 +7,7 @@ localesdb = dbname.locale # DB for localization
 group_types: Iterable[ChatType] = (ChatType.GROUP, ChatType.SUPERGROUP)
 
 async def set_db_lang(chat_id: int, chat_type: str, lang_code: str):
-    await localesdb.update_one({"chat_id": chat_id}, {"$set": {"lang": lang_code, "chat_type": chat_type}}, upsert=True)
+    await localesdb.update_one({"chat_id": chat_id}, {"$set": {"lang": lang_code, "chat_type": chat_type.value}}, upsert=True)
 
 
 async def get_db_lang(chat_id: int, chat_type: str) -> str:
