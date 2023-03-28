542679de1a193fa25b000735ec72695d3a316379
yasir <yasiramunandar@gmail.com>
2023-03-28 12:01:51 +0700

    Tess2

--- misskaty/plugins/ocr.py
+++ misskaty/plugins/ocr.py
@@ -19,7 +19,7 @@ from misskaty.helper.http import http
 from misskaty.vars import COMMAND_HANDLER
 
 __MODULE__ = "OCR"
-__HELP__ = "/ocr [reply to photo] - Read Text From Image"
+__HELP__ = HELP_MSG
 
 
 @app.on_message(filters.command(["ocr"], COMMAND_HANDLER))
