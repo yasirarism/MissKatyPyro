8abf02d705cde455ef7c67cb1eb355dbd101c8bc
yasir <yasiramunandar@gmail.com>
2023-03-28 12:01:39 +0700

    Tess2

--- misskaty/plugins/ocr.py
+++ misskaty/plugins/ocr.py
@@ -27,6 +27,8 @@ __HELP__ = "/ocr [reply to photo] - Read Text From Image"
 @ratelimiter
 @use_chat_lang()
 async def ocr(_, m, strings):
+    global HELP_MSG
+    HELP_MSG = strings("ocr_helper")
     reply = m.reply_to_message
     if not reply or not reply.photo and not reply.sticker:
         return await kirimPesan(m, strings("no_photo").format(cmd=m.command[0]), quote=True)
