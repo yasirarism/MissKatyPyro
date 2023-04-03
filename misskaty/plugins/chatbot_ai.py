cf1562de121aa14bc2a3d76edf83b1eefafe57a9
yasir <yasiramunandar@gmail.com>
2023-04-03 08:12:02 +0700

    fixx

--- misskaty/plugins/chatbot_ai.py
+++ misskaty/plugins/chatbot_ai.py
@@ -5,11 +5,12 @@ from pyrogram.errors import MessageTooLong
 
 from misskaty import app
 from misskaty.helper.localization import use_chat_lang
-from misskaty.helper import http, post_to_telegraph
+from misskaty.helper import http, post_to_telegraph, check_time_gap
 from misskaty.core.message_utils import *
 from misskaty.core.decorator.ratelimiter import ratelimiter
-from misskaty.vars import COMMAND_HANDLER, OPENAI_API
+from misskaty.vars import COMMAND_HANDLER, OPENAI_API, SUDO
 
+openai.api_key = OPENAI_API
 
 @app.on_message(filters.command("ask", COMMAND_HANDLER))
 @ratelimiter
@@ -17,7 +18,9 @@ from misskaty.vars import COMMAND_HANDLER, OPENAI_API
 async def chatbot(c, m, strings):
     if len(m.command) == 1:
         return await kirimPesan(m, strings("no_question").format(cmd=m.command[0]), quote=True)
-    openai.api_key = OPENAI_API
+    is_in_gap, sleep_time = await check_time_gap(m.from_user.id or m.sender_chat.id)
+    if is_in_gap and (m.from_user.id or m.sender_chat.id not in SUDO):
+        return await kirimPesan(m, strings("dont_spam"))
     openai.aiosession.set(ClientSession())
     pertanyaan = m.text.split(" ", maxsplit=1)[1]
     msg = await kirimPesan(m, strings("find_answers_str"), quote=True)
@@ -37,7 +40,7 @@ async def chatbot(c, m, strings):
                 continue
             num += 1
             answer +=  chunk.choices[0].delta.content
-            if num == 20:
+            if num == 30:
                 await editPesan(msg, answer)
                 await asyncio.sleep(1)
                 num = 0
