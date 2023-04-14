d0397307d42da1163cd459084939286eba2be9c1
yasir <yasiramunandar@gmail.com>
2023-04-14 12:44:51 +0700

    Tes

--- misskaty/core/misskaty_patch/bound/message.py
+++ misskaty/core/misskaty_patch/bound/message.py
@@ -1,22 +1,101 @@
 import io
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup
 from pyrogram.sync import async_to_sync
+from pyrogram import enums
+from datetime import datetime
 from asyncio import sleep as asleep, get_event_loop
-from typing import Union
+from typing import Union, Optional
 from misskaty import app, user
 from ..types.send_as_file import send_as_file
 
-Message.input = property(
-    lambda m: m.text[m.text.find(m.command[0]) + len(m.command[0]) + 1:] 
-    if len(m.command) > 1 else None
-)
+async def input_str(self) -> str:
+    input_ = self.text
+    if ' ' in input_ or '\n' in input_:
+        return str(input_.split(maxsplit=1)[1].strip())
+    return ''
+
 
 async def del_in(self: Message, seconds: int, revoke: bool=True):
     """Delete message in x Seconds"""
     await asleep(seconds)
     return await self.delete(revoke=revoke)
 
-async def reply_as_file(self, client: Union['app', 'user'], text: str, filename: str = "output.txt", caption: str = '', delete_message: bool = True):
+async def reply(self: Message,
+                text: str,
+                del_in: int = -1,
+                quote: Optional[bool] = None,
+                parse_mode: Optional[enums.ParseMode] = None,
+                disable_web_page_preview: Optional[bool] = None,
+                disable_notification: Optional[bool] = None,
+                reply_to_message_id: Optional[int] = None,
+                schedule_date: Optional[datetime] = None,
+                protect_content: Optional[bool] = None,
+                reply_markup: InlineKeyboardMarkup = None) -> Union['Message', bool]:
+        """\nExample:
+                message.reply("hello")
+        Parameters:
+            text (``str``):
+                Text of the message to be sent.
+            del_in (``int``):
+                Time in Seconds for delete that message.
+            log (``bool`` | ``str``, *optional*):
+                If ``True``, the message will be forwarded
+                to the log channel.
+                If ``str``, the logger name will be updated.
+            quote (``bool``, *optional*):
+                If ``True``, the message will be sent as
+                a reply to this message.
+                If *reply_to_message_id* is passed,
+                this parameter will be ignored.
+                Defaults to ``True`` in group chats
+                and ``False`` in private chats.
+            parse_mode (:obj:`enums.ParseMode`, *optional*):
+                By default, texts are parsed using both
+                Markdown and HTML styles.
+                You can combine both syntaxes together.
+                Pass "markdown" or "md" to enable
+                Markdown-style parsing only.
+                Pass "html" to enable HTML-style parsing only.
+                Pass None to completely disable style parsing.
+            disable_web_page_preview (``bool``, *optional*):
+                Disables link previews for links in this message.
+            disable_notification (``bool``, *optional*):
+                Sends the message silently.
+                Users will receive a notification with no sound.
+            reply_to_message_id (``int``, *optional*):
+                If the message is a reply, ID of the original message.
+            schedule_date (:py:obj:`~datetime.datetime`, *optional*):
+                Date when the message will be automatically sent. Unix time.
+            protect_content (``bool``, *optional*):
+                Protects the contents of the sent message from forwarding and saving.
+            reply_markup (:obj:`InlineKeyboardMarkup`
+            | :obj:`ReplyKeyboardMarkup` | :obj:`ReplyKeyboardRemove`
+            | :obj:`ForceReply`, *optional*):
+                Additional interface options. An object for an inline keyboard,
+                custom reply keyboard,
+                instructions to remove reply keyboard or to
+                force a reply from the user.
+        Returns:
+            On success, the sent Message or True is returned.
+        Raises:
+            RPCError: In case of a Telegram RPC error.
+        """
+        if quote is None:
+            quote = self.chat.type != enums.ChatType.PRIVATE
+        if reply_to_message_id is None and quote:
+            reply_to_message_id = self.id
+        return await self.reply_text(text=text,
+                                               del_in=del_in,
+                                               parse_mode=parse_mode,
+                                               disable_web_page_preview=disable_web_page_preview,
+                                               disable_notification=disable_notification,
+                                               reply_to_message_id=reply_to_message_id,
+                                               schedule_date=schedule_date,
+                                               protect_content=protect_content,
+                                               reply_markup=reply_markup)
+
+
+async def reply_as_file(self, text: str, filename: str = "output.txt", caption: str = '', delete_message: bool = True):
         """\nYou can send large outputs as file
         Example:
             message.reply_as_file(text="hello")
@@ -43,6 +122,8 @@ async def reply_as_file(self, client: Union['app', 'user'], text: str, filename:
                                         disable_notification=True,
                                         reply_to_message_id=reply_to_id)
 
+Message.input = input_str
+Message.reply_text = reply
 Message.reply_as_file = reply_as_file
 Message.delete_in = del_in
 async_to_sync(Message, 'delete_in')
