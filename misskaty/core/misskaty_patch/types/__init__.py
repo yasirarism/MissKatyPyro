5380dba99815110da3f7d4ea8b27a8bb68805f5b
yasir <yasiramunandar@gmail.com>
2023-04-14 14:06:46 +0700

    Tes

--- misskaty/core/misskaty_patch/types/__init__.py
+++ misskaty/core/misskaty_patch/types/__init__.py
@@ -1,7 +1,3 @@
 # from .send_message import SendMessage
 # from .edit_message_text import EditMessageText
-from .send_as_file import SendAsFile
-
-
-class Messages(SendAsFile):
-    """ methods.messages """
+from .send_as_file import send_as_file
