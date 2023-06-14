203d3e21b8eca3833b1038df1186ee67aa4834f1
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-06-14 15:37:45 +0000

    Fix lagi

--- misskaty/plugins/ban_user_or_chat.py
+++ misskaty/plugins/ban_user_or_chat.py
@@ -12,7 +12,7 @@ async def ban_reply(self: Client, ctx: Message):
     if not ctx.from_user:
         return
     ban = await db.get_ban_status(ctx.from_user.id)
-    if ban.get("is_banned") and (ctx.chat.type.value == "private" or (ctx.chat.type.value == "supergroup" and ctx.command)):
+    if (ban.get("is_banned") and ctx.chat.type.value == "private") or (ban.get("is_banned") and ctx.chat.type.value == "supergroup" and ctx.command):
         await ctx.reply_msg(f'I am sorry, You are banned to use Me. \nBan Reason: {ban["ban_reason"]}')
         await ctx.stop_propagation()
 
