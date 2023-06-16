9fb228c48cadc789e4aeae11134e9db0f124bb5e
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-06-17 00:04:34 +0700

    Fix space in readable time

--- misskaty/helper/human_read.py
+++ misskaty/helper/human_read.py
@@ -19,17 +19,17 @@ def get_readable_time(seconds: int) -> str:
     (days, remainder) = divmod(seconds, 86400)
     days = int(days)
     if days != 0:
-        result += f"{days} d "
+        result += f"{days}d "
     (hours, remainder) = divmod(remainder, 3600)
     hours = int(hours)
     if hours != 0:
-        result += f"{hours} h "
+        result += f"{hours}h "
     (minutes, seconds) = divmod(remainder, 60)
     minutes = int(minutes)
     if minutes != 0:
-        result += f"{minutes} m "
+        result += f"{minutes}m "
     seconds = int(seconds)
-    result += f"{seconds} s "
+    result += f"{seconds}s "
     return result
 
 
