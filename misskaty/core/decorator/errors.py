a838e592f39dbdda857b8a984b08453335999483
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-06-22 07:44:57 +0700

    Update errors.py

--- misskaty/core/decorator/errors.py
+++ misskaty/core/decorator/errors.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import traceback
-from functools import wraps, partial
+from functools import wraps
 from datetime import datetime
 
 from pyrogram.types import CallbackQuery
@@ -16,11 +16,11 @@ def capture_err(func):
     @wraps(func)
     async def capture(client, message, *args, **kwargs):
         if isinstance(message, CallbackQuery):
-            sender = partial(message.answer, show_alert=True)
+            sender = message.message.reply
             chat = message.message.chat
             msg = message.message.text or message.message.caption
         else:
-            sender = message.reply_text
+            sender = message.reply
             chat = message.chat
             msg = message.text or message.caption
         try:
@@ -39,7 +39,7 @@ def capture_err(func):
             tgl_now = datetime.now()
 
             cap_day = f"{day.strftime('%A')}, {tgl_now.strftime('%d %B %Y %H:%M:%S')}"
-            await sender.reply("😭 An Internal Error Occurred while processing your Command, the Logs have been sent to the Owners of this Bot. Sorry for Inconvenience...")
+            await sender("😭 An Internal Error Occurred while processing your Command, the Logs have been sent to the Owners of this Bot. Sorry for Inconvenience...")
             with open(f"crash_{tgl_now.strftime('%d %B %Y')}.txt", "w+", encoding="utf-8") as log:
                 log.write(error_feedback)
                 log.close()
