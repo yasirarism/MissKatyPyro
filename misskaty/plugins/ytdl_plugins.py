7af42ce6d524d7593ca5f14519a66d8eb7acc283
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-07-01 22:54:05 +0700

    ok

--- misskaty/plugins/ytdl_plugins.py
+++ misskaty/plugins/ytdl_plugins.py
@@ -103,15 +103,9 @@ async def ytdownv2(self: Client, ctx: Message, strings):
             caption = x.caption
             markup = x.buttons
             photo = x.image_url
-            msg = await ctx.reply_photo(
+            await ctx.reply_photo(
                 photo, caption=caption, reply_markup=markup, quote=True
             )
-            await msg.wait_for_click(from_user_id=ctx.from_user.id, timeout=30)
-        except ListenerTimeout:
-            try:
-                await msg.edit_caption(strings("exp_task", context="general"))
-            except MessageIdInvalid:
-                pass
         except Exception as err:
             await ctx.reply_msg(f"Opps, ERROR: {str(err)}")
 
