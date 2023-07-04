503c0dd6c3c4410c2cfa3c8ccfa4c7ad7993f747
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-07-04 12:18:01 +0700

    raoi

--- misskaty/plugins/misc_tools.py
+++ misskaty/plugins/misc_tools.py
@@ -65,7 +65,9 @@ async def kbbi_search(_, ctx: Client):
     for a in r.get("result"):
         submakna = "".join(f"{a}, " for a in a['makna'][0]['submakna'])[:-2]
         contoh = "".join(f"{a}, " for a in a['makna'][0]['contoh'])[:-2]
-        res += f"<b>{a['nama']} ({a['makna'][0]['kelas'][0]['nama']}: {a['makna'][0]['kelas'][0]['deskripsi']})</b>\n<b>Kata Dasar:</b> {a['kata_dasar'] if a['kata_dasar'] else '-'}\n<b>Bentuk Tidak Baku:</b> {a['bentuk_tidak_baku'] if a['bentuk_tidak_baku'] else '-'}\n<b>Submakna:</b> {submakna}\n<b>Contoh:</b> {contoh if contoh else '-'}\n\n"
+        kt_dasar = "".join(f"{a}, " for a in a['kata_dasar'])[:-2]
+        bt_takbaku = "".join(f"{a}, " for a in a['bentuk_tidak_baku'])[:-2]
+        res += f"<b>{a['nama']} ({a['makna'][0]['kelas'][0]['nama']}: {a['makna'][0]['kelas'][0]['deskripsi']})</b>\n<b>Kata Dasar:</b> {kt_dasar if kt_dasar else '-'}\n<b>Bentuk Tidak Baku:</b> {bt_takbaku if bt_takbaku else '-'}\n<b>Submakna:</b> {submakna}\n<b>Contoh:</b> {contoh if contoh else '-'}\n\n"
     await ctx.reply(res)
 
 
