c015f754cd6b117321135a2da335b679eb67e2c5
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-07-20 12:24:27 +0700

    Unused variable 'i'

--- misskaty/plugins/auto_approve.py
+++ misskaty/plugins/auto_approve.py
@@ -60,7 +60,7 @@ async def approve_chat(c, q):
 @app.on_callback_query(filters.regex(r"^declined"))
 @ratelimiter
 async def decline_chat(c, q):
-    i, chat = q.data.split("_")
+    _, chat = q.data.split("_")
     try:
         await q.message.edit(
             "Yahh, kamu ditolak join channel. Biasakan rajin membaca yahhh.."
