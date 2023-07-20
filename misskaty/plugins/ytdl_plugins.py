841d6739e3a5f636cb1b2804132ce6fafae497a3
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-07-20 13:26:16 +0700

    lupa

--- misskaty/plugins/ytdl_plugins.py
+++ misskaty/plugins/ytdl_plugins.py
@@ -19,7 +19,8 @@ from pyrogram.types import (
 )
 
 from misskaty import app
-from misskaty.core import capture_err, pyro_cooldown, ratelimiter
+from misskaty.core import pyro_cooldown
+from misskaty.core.decorator import capture_err, ratelimiter
 from misskaty.helper.http import http
 from misskaty.helper.localization import use_chat_lang
 from misskaty.vars import COMMAND_HANDLER, LOG_CHANNEL
