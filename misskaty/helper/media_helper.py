7a6bb18842979dc63af4c5fa2c01a5c9c357199d
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-07-20 14:33:26 +0700

    Update media_helper.py

--- misskaty/helper/media_helper.py
+++ misskaty/helper/media_helper.py
@@ -13,10 +13,10 @@ async def post_to_telegraph(is_media: bool, title=None, content=None, media=None
     if telegraph.get_access_token() is None:
         await telegraph.create_account(short_name=BOT_USERNAME)
     if is_media:
-        '''Create a Telegram Post Foto/Video'''
+        # Create a Telegram Post Foto/Video
         response = await telegraph.upload_file(media)
         return f"https://telegra.ph{response[0]['src']}"
-    '''Create a Telegram Post using HTML Content'''
+    # Create a Telegram Post using HTML Content
     response = await telegraph.create_page(
         title,
         html_content=content,
