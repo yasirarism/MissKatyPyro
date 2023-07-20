b49db692c26a5b11667eeff640806612964bea94
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-07-20 13:40:53 +0700

    Update __init__.py

--- misskaty/core/decorator/__init__.py
+++ misskaty/core/decorator/__init__.py
@@ -1,5 +1,6 @@
-# skipcq
 from .errors import capture_err
 from .misc import asyncify, new_task
 from .permissions import adminsOnly, require_admin
 from .ratelimiter import ratelimiter
+
+__all__ = capture_err, asyncify, new_task, adminsOnly, require_admin, ratelimiter
