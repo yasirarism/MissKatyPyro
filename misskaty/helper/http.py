cfc3a0afc99f52ec94a6a61397fa7c4b7eeb6753
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-08-19 21:46:42 +0700

    Ohh koma

--- misskaty/helper/http.py
+++ misskaty/helper/http.py
@@ -10,7 +10,7 @@ session = ClientSession()
 http = httpx.AsyncClient(
     http2=True,
     verify=False,
-    headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42"}
+    headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42"},
     timeout=httpx.Timeout(10),
 )
 
