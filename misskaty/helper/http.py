a3ea2da3deed4fda369deb59e42f0ed24dc4f1c0
yasirarism <github@yasir.eu.org>
2023-09-06 20:40:36 +0700

    Increase httpx timeout

--- misskaty/helper/http.py
+++ misskaty/helper/http.py
@@ -14,7 +14,7 @@ fetch = httpx.AsyncClient(
         "Accept-Language": "id-ID",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42",
     },
-    timeout=httpx.Timeout(10),
+    timeout=httpx.Timeout(20),
 )
 
 
