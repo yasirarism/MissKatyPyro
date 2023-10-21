6c763e1a2803a6b5a48eab720981c0e202d432a7
Yasir Aris M <git@yasirdev.my.id>
2023-10-21 23:38:27 +0700

    Change httpx setting to English
    Signed-off-by: Yasir Aris M <git@yasirdev.my.id>

--- misskaty/helper/http.py
+++ misskaty/helper/http.py
@@ -11,7 +11,7 @@ fetch = AsyncClient(
     http2=True,
     verify=False,
     headers={
-        "Accept-Language": "id-ID",
+        "Accept-Language": "en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42",
     },
     timeout=Timeout(20),
