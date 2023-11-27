68e65e2ce5c0d4909098de1fe6a7051bcc74e6ec
Yasir Aris M <git@yasirdev.my.id>
2023-11-28 06:55:00 +0700

    Debug
    Signed-off-by: Yasir Aris M <git@yasirdev.my.id>

--- misskaty/plugins/media_extractor.py
+++ misskaty/plugins/media_extractor.py
@@ -121,7 +121,8 @@ async def ceksub(_, ctx: Message, strings):
             await msg.wait_for_click(from_user_id=ctx.from_user.id, timeout=30)
         except ListenerTimeout:
             await msg.edit_msg(strings("exp_task", context="general"))
-    except Exception:
+    except Exception as e:
+        self.log.info(e)
         await pesan.edit_msg(strings("fail_extr_media"))
 
 
