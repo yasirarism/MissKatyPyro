64c6e3a1a0056c941c5a2a5724e38b060a0828e3
Yasir Aris M <git@yasirdev.my.id>
2024-05-29 13:37:50 +0700

    Change Payment Link Donation
    Signed-off-by: Yasir Aris M <git@yasirdev.my.id>

--- misskaty/plugins/dev.py
+++ misskaty/plugins/dev.py
@@ -130,7 +130,7 @@ async def donate(self: Client, ctx: Message):
         await ctx.react(emoji="❤️")
     try:
         await ctx.reply_photo(
-            "https://img.yasirweb.eu.org/file/9427d61d6968b8ee4fb2f.jpg",
+            "https://img.yasirweb.eu.org/file/1535d96d6fbeeb9556183.jpg",
             caption=f"Hi {ctx.from_user.mention}, If you find this bot useful, you can make a donation to the account below. Because this bot server uses VPS and is not free. Thank You..\n\n<b>Indonesian Payment:</b>\n<b>QRIS:</b> https://img.yasirweb.eu.org/file/b1c86973ae4e55721983a.jpg (Yasir Store)\n<b>Mayar:</b> https://yasirarism.mayar.link/payme\n<b>Bank Jago:</b> 109641845083 (Yasir Aris M)\n\nFor international people can use PayPal to support me or via GitHub Sponsor:\nhttps://paypal.me/yasirarism\nhttps://github.com/sponsors/yasirarism\n\n<b>Source:</b> @BeriKopi",
         )
     except (ChatSendPlainForbidden, ChatSendPhotosForbidden):
