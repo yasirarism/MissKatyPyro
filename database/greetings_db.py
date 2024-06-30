36ddaa6d2b76b555587b78ee7f4860394a62387c
Yasir Aris M <git@yasirdev.my.id>
2024-06-30 21:49:46 +0700

    Penyederhanaan by gpt 4

--- database/greetings_db.py
+++ database/greetings_db.py
@@ -3,17 +3,12 @@ from database import dbname
 greetingdb = dbname["greetings"]
 
 async def is_welcome(chat_id: int) -> bool:
-    greets = await greetingdb.find_one({"chat_id": chat_id})
-    return bool(greets)
+    return bool(await greetingdb.find_one({"chat_id": chat_id}))
 
-async def welcome_off(chat_id: int):
-    wlc = await greetingdb.find_one({"chat_id": chat_id})
-    if wlc:
-        return await cleandb.delete_one({"chat_id": chat_id})
-
-async def welcome_on(chat_id: int):
-    wlc = await greetingdb.find_one({"chat_id": chat_id})
-    if not wlc:
-        return await greetingdb.insert_one({"chat_id": chat_id})
+async def toggle_welcome(chat_id: int):
+    if await is_welcome(chat_id):
+        await greetingdb.delete_one({"chat_id": chat_id})
+    else:
+        await greetingdb.insert_one({"chat_id": chat_id})
 
 # todo other features for custom welcome here
