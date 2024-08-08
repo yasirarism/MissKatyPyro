8837cdeed02e4ecd73d6a40a76d5f53bf4b38fc1
Yasir Aris M <git@yasirdev.my.id>
2024-08-08 10:30:13 +0700

    Fix http helper

--- misskaty/helper/http.py
+++ misskaty/helper/http.py
@@ -1,14 +1,9 @@
 from asyncio import gather
-
-from aiohttp import ClientSession
 from httpx import AsyncClient, Timeout
-
-# Aiohttp Async Client
-# session = ClientSession()
 
 # HTTPx Async Client
 fetch = AsyncClient(
-    http2=True,
+    https2=True
     verify=False,
     headers={
         "Accept-Language": "en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7",
@@ -19,29 +14,29 @@ fetch = AsyncClient(
 
 
 async def get(url: str, *args, **kwargs):
-    async with session.get(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
+    try:
+        resp = await fetch.get(url, *args, **kwargs)
+        data = await resp.json()
+    except Exception:
+        data = await resp.text()
     return data
 
 
 async def head(url: str, *args, **kwargs):
-    async with session.head(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
+    try:
+        resp = await fetch.head(url, *args, **kwargs)
+        data = await resp.json()
+    except Exception:
+        data = await resp.text()
     return data
 
 
 async def post(url: str, *args, **kwargs):
-    async with session.post(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
+    try:
+        resp = await fetch.post(url, *args, **kwargs)
+        data = await resp.json()
+    except Exception:
+        data = await resp.text()
     return data
 
 
@@ -58,8 +53,8 @@ async def multipost(url: str, times: int, *args, **kwargs):
 
 
 async def resp_get(url: str, *args, **kwargs):
-    return await session.get(url, *args, **kwargs)
+    return await fetch.get(url, *args, **kwargs)
 
 
 async def resp_post(url: str, *args, **kwargs):
-    return await session.post(url, *args, **kwargs)
+    return await fetch.post(url, *args, **kwargs)
