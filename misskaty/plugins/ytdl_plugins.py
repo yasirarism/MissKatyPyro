ded5b96a9657d52dd35c6265f71e1ac73b5db2f9
Yasir Aris M <git@yasirdev.my.id>
2024-09-26 12:07:44 +0700

    typo

--- misskaty/plugins/ytdl_plugins.py
+++ misskaty/plugins/ytdl_plugins.py
@@ -198,7 +198,7 @@ async def ytdl_gendl_callback(self: Client, cq: CallbackQuery, strings):
             return await cq.answer(strings("unauth"), True)
         except QueryIdInvalid:
             return
-    if match[2] in ["mkv", "mp4"] and cq.from_user.id not in SUDO and and cq.from_user.id == OWNER_ID:
+    if match[2] in ["mkv", "mp4"] and cq.from_user.id not in SUDO and cq.from_user.id == OWNER_ID:
         try:
             return await cq.answer(strings("vip-btn"), True)
         except QueryIdInvalid:
