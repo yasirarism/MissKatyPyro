c5ac96deff45b149f8304eb7cbe64427ebde335f
yasirarism <yasirarism@users.noreply.github.com>
2026-08-02 06:35:06 +0000

    refactor: media_extractor class-based + fix button callback bugs

--- misskaty/plugins/media_extractor.py
+++ misskaty/plugins/media_extractor.py
@@ -5,6 +5,7 @@
 * Copyright @YasirPedia All rights reserved
 """
 
+import contextlib
 import json
 import os
 import traceback
@@ -62,7 +63,7 @@ def get_base_name(orig_path: str):
 
 
 def get_subname(lang, url, ext):
-    fragment_removed = url.split("#")[0]  # keep to left of first #
+    fragment_removed = url.split("#")[0]
     query_string_removed = fragment_removed.split("?")[0]
     scheme_removed = query_string_removed.split("://")[-1].split(":")[-1]
     if scheme_removed.find("/") == -1 or not get_base_name(
@@ -72,146 +73,115 @@ def get_subname(lang, url, ext):
     return f"[{lang.upper()}] {get_base_name(os.path.basename(unquote(scheme_removed)))}{get_random_string(3)}.{ext}"
 
 
-@app.on_message(filters.command(["ceksub", "extractmedia"], COMMAND_HANDLER))
-@use_chat_lang()
-async def ceksub(self, ctx: Message, strings):
-    if len(ctx.command) == 1:
-        return await ctx.reply(
-            strings("sub_extr_help").format(cmd=ctx.command[0]), del_in=5
-        )
-    link = ctx.command[1]
-    start_time = time()
-    pesan = await ctx.reply(strings("progress_str"))
-    try:
-        res = (
-            await shell_exec(
-                f"ffprobe -loglevel 0 -print_format json -show_format -show_streams {link}"
-            )
-        )[0]
-        details = json.loads(res)
-        buttons = []
-        for stream in details["streams"]:
-            mapping = stream["index"]
+class MediaExtractor:
+    def __init__(self, client: Client, message: Message | None = None, callback: CallbackQuery | None = None, strings=None) -> None:
+        self.client = client
+        self.message = message
+        self.callback = callback
+        self.strings = strings
+
+    async def _reply(self, text: str, *args, **kwargs):
+        if self.callback:
+            return await self.callback.answer(text, *args, **kwargs)
+        if self.message:
+            return await self.message.reply_text(text, *args, **kwargs)
+        return None
+
+    async def _edit(self, text: str, *args, **kwargs):
+        target = getattr(self.callback, "message", None) or self.message
+        if not target:
+            return None
+        return await target.edit_text(text, *args, **kwargs)
+
+    async def _progress_reply(self, text: str):
+        if self.callback:
+            return await self.callback.message.edit(text)
+        return await self.message.reply_text(text)
+
+    async def _delete_progress(self):
+        if self.callback and self.callback.message:
+            with contextlib.suppress(Exception):
+                await self.callback.message.delete()
+        elif self.message:
+            with contextlib.suppress(Exception):
+                await self.message.delete()
+
+    def _is_authorized(self, user_id: int) -> bool:
+        if self.callback:
+            usr = self.callback.message.reply_to_message
+            if not usr or not usr.from_user:
+                return False
+            return user_id == usr.from_user.id
+        return True
+
+    def _get_source_link(self) -> str | None:
+        if self.callback:
             try:
-                stream_name = stream["codec_name"]
+                return self.callback.message.reply_to_message.command[1]
             except Exception:
-                stream_name = "-"
-            stream_type = stream["codec_type"]
-            if stream_type not in ("audio", "subtitle"):
-                continue
+                return None
+        if self.message:
             try:
-                lang = stream["tags"]["language"]
+                return self.message.command[1]
             except Exception:
-                lang = mapping
-            buttons.append(
-                [
+                return None
+        return None
+
+    async def analyze(self, link: str):
+        try:
+            res = (await shell_exec(f"ffprobe -loglevel 0 -print_format json -show_format -show_streams {link}"))[0]
+            details = json.loads(res)
+            buttons = []
+            for stream in details.get("streams", []):
+                mapping = stream.get("index", "?")
+                stream_name = stream.get("codec_name", "-")
+                stream_type = stream.get("codec_type", "")
+                if stream_type not in ("audio", "subtitle"):
+                    continue
+                lang = stream.get("tags", {}).get("language", str(mapping))
+                safe_lang = str(lang).replace("#", "_")
+                safe_codec = str(stream_name).replace("#", "_")
+                callback_data = f"streamextract#{safe_lang}#0:{mapping}#{safe_codec}"
+                buttons.append([
                     InlineKeyboardButton(
                         f"0:{mapping}({lang}): {stream_type}: {stream_name}",
-                        f"streamextract#{lang}#0:{mapping}#{stream_name}",
+                        callback_data,
                     )
-                ]
+                ])
+            buttons.append([
+                InlineKeyboardButton(self.strings("cancel_btn"), f"close#{self.message.from_user.id if self.message and self.message.from_user else 'unknown'}")
+            ])
+            return buttons
+        except Exception as e:
+            LOGGER.error(f"Analyze media failed: {e}")
+            raise
+
+    async def extract(self, map_code: str, lang: str, codec: str, link: str):
+        ext = {
+            "aac": "aac",
+            "mp3": "mp3",
+            "eac3": "eac3",
+        }.get(codec, "srt")
+        namafile = get_subname(lang, link, ext)
+        start_time = time()
+        try:
+            LOGGER.info(f"ExtractSub: {namafile} by {self.callback.from_user.first_name} [{self.callback.from_user.id}]")
+            await shell_exec(f"ffmpeg -i {link} -map {map_code} '{namafile}'")
+            timelog = time() - start_time
+            c_time = time()
+            usr = self.callback.message.reply_to_message
+            await self.callback.message.reply_document(
+                namafile,
+                caption=self.strings("capt_extr_sub").format(
+                    nf=namafile, bot=self.client.me.username, timelog=get_readable_time(timelog)
+                ),
+                reply_parameters=pyro_types.ReplyParameters(message_id=usr.id),
+                thumb="assets/thumb.jpg",
+                progress=progress_for_pyrogram,
+                progress_args=(self.strings("up_str"), self.callback.message, c_time, self.client.me.dc_id),
             )
-        timelog = time() - start_time
-        buttons.append(
-            [InlineKeyboardButton(strings("cancel_btn"), f"close#{ctx.from_user.id}")]
-        )
-        await pesan.edit(
-            strings("press_btn_msg").format(timelog=get_readable_time(timelog)),
-            reply_markup=InlineKeyboardMarkup(buttons),
-        )
-    except Exception as e:
-        self.log.info(traceback.print_exc())
-        await pesan.edit(strings("fail_extr_media"))
-
-
-@app.on_message(filters.command(["converttosrt", "converttoass"], COMMAND_HANDLER))
-@capture_err
-@use_chat_lang()
-async def convertsrt(self: Client, ctx: Message, strings):
-    reply = ctx.reply_to_message
-    if (
-        not reply
-        or not reply.document
-        or not reply.document.file_name
-        or not reply.document.file_name.endswith((".vtt", ".ass", ".srt"))
-    ):
-        return await ctx.reply(
-            strings("conv_sub_help").format(cmd=ctx.command[0]), del_in=6
-        )
-    msg = await ctx.reply(strings("convert_str"))
-    if not os.path.exists("downloads"):
-        os.makedirs("downloads")
-    dl = await reply.download(file_name="downloads/")
-    filename = dl.split("/", 3)[3]
-    LOGGER.info(
-        f"ConvertSub: {filename} by {ctx.from_user.first_name if ctx.from_user else ctx.sender_chat.title} [{ctx.from_user.id if ctx.from_user else ctx.sender_chat.id}]"
-    )
-    suffix = "srt" if ctx.command[0] == "converttosrt" else "ass"
-    await shell_exec(f"ffmpeg -i '{dl}' 'downloads/{filename}.{suffix}'")
-    c_time = time()
-    await ctx.reply_document(
-        f"downloads/{filename}.{suffix}",
-        caption=strings("capt_conv_sub").format(nf=filename, bot=self.me.username),
-        thumb="assets/thumb.jpg",
-        progress=progress_for_pyrogram,
-        progress_args=(strings("up_str"), msg, c_time, self.me.dc_id),
-    )
-    await msg.delete_msg()
-    try:
-        os.remove(dl)
-        os.remove(f"downloads/{filename}.{suffix}")
-    except Exception:
-        pass
-
-
-@app.on_callback_query(filters.regex(r"^streamextract#"))
-@use_chat_lang()
-async def stream_extract(self: Client, update: CallbackQuery, strings):
-    cb_data = update.data
-    usr = update.message.reply_to_message
-    if update.from_user.id != usr.from_user.id:
-        return await update.answer(strings("unauth_cb"), True)
-    _, lang, map_code, codec = cb_data.split("#")
-    try:
-        link = update.message.reply_to_message.command[1]
-    except Exception:
-        return await update.answer(strings("invalid_cb"), True)
-    await update.message.edit(strings("progress_str"))
-    if codec == "aac":
-        ext = "aac"
-    elif codec == "mp3":
-        ext = "mp3"
-    elif codec == "eac3":
-        ext = "eac3"
-    else:
-        ext = "srt"
-    start_time = time()
-    namafile = get_subname(lang, link, ext)
-    try:
-        LOGGER.info(
-            f"ExtractSub: {namafile} by {update.from_user.first_name} [{update.from_user.id}]"
-        )
-        (await shell_exec(f"ffmpeg -i {link} -map {map_code} '{namafile}'"))[0]
-        timelog = time() - start_time
-        c_time = time()
-        await update.message.reply_document(
-            namafile,
-            caption=strings("capt_extr_sub").format(
-                nf=namafile, bot=self.me.username, timelog=get_readable_time(timelog)
-            ),
-            reply_parameters=pyro_types.ReplyParameters(message_id=usr.id),
-            thumb="assets/thumb.jpg",
-            progress=progress_for_pyrogram,
-            progress_args=(strings("up_str"), update.message, c_time, self.me.dc_id),
-        )
-        await update.message.delete_msg()
-        try:
-            os.remove(namafile)
-        except Exception:
-            pass
-    except Exception as e:
-        try:
-            os.remove(namafile)
-        except Exception:
-            pass
-        await update.message.edit(strings("fail_extr_sub").format(link=link, e=e))
+            await self._delete_progress()
+        except Exception as e:
+            with contextlib.suppress(Exception):
+                os.remove(namafile)
+            await self._edit(self.strings("fail_extr_sub").format(link=link, e=e))
